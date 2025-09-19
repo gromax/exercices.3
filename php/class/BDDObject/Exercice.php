a8601333d219f7809cbfbda3df1d8e25bd615476
gromax <gromaxx@gmail.com>
2025-09-19 23:46:18 +0200

    nouvelle table

---
+++ php/class/BDDObject/Exercice.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BDDObject;
+
+use PDO;
+use PDOException;
+use ErrorController as EC;
+use SessionController as SC;
+
+final class Exercice extends Item
+{
+    protected static $BDDName = "exercices";
+
+    ##################################### METHODES STATIQUES #####################################
+
+    protected static function champs()
+    {
+        return array(
+            'nom' => array( 'def' => "", 'type'=> 'string'),        // nom de l'exercice
+            'keywords' => array( 'def' => "", 'type'=> 'string'),   // mots-clés de l'exercice
+            'descriptif' => array( 'def' => "", 'type'=> 'string'), // descriptif de l'exercice
+            'options' => array( 'def' => "", 'type'=> 'string'),   // options de l'exercice (json)
+            'data' => array( 'def' => "", 'type'=> 'string'),      // code de l'exercice (json)
+            );
+    }
+
+    public static function getList()
+    {
+        require_once BDD_CONFIG;
+        try {
+            $pdo=new PDO(BDD_DSN,BDD_USER,BDD_PASSWORD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $pdo->prepare("SELECT id, nom, descriptif, keywords, options, data FROM (".PREFIX_BDD."exercices");
+            $stmt->execute();
+            $bdd_result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            EC::addBDDError($e->getMessage(), "Exercices/getList");
+            return array("error"=>true, "message"=>$e->getMessage());
+        }
+        return $bdd_result;
+    }
+
+    ##################################### METHODES #####################################
+}
+
+?>
