b900ee0b386774a6d2f2bfec10f3b76e9934c243
gromax <gromaxx@gmail.com>
2025-09-20 22:09:55 +0200

    premier commit

---
+++ app/components/exercices/list/views.js
@@ -0,0 +1,54 @@
+import { View, CollectionView } from 'backbone.marionette'
+import { FilterList, FilterPanel } from '../../behaviors.js'
+
+import panel_tpl from 'templates/exercices/list/exercice-list-panel.jst'
+import no_exercice_view_tpl from 'templates/exercices/list/exercice-list-none.jst'
+import exercice_item_view_tpl from 'templates/exercices/list/exercice-list-item.jst'
+import exercices_list_layout_tpl from '@templates/exercices/list/exercices-list-layout.jst';
+
+const ListLayout = View.extend ({
+  template: exercices_list_layout_tpl,
+  regions: {
+    panelRegion: "#panel-region",
+    itemsRegion: "#items-region"
+  }
+});
+
+const ExercicesPanel = View.extend({
+  template: panel_tpl,
+  behaviors: [FilterPanel],
+  templateContext() {
+    return {
+      filterCriterion: this.getOption("filterCriterion")
+    };
+  }
+});
+
+const NoExerciceView = View.extend({
+  template: no_exercice_view_tpl,
+  tagName: "a",
+  className: "list-group-item"
+});
+
+const ExerciceItemView = View.extend({
+  tagName: "a",
+  className: "list-group-item",
+  template: exercice_item_view_tpl,
+  triggers: {
+    "click": "exercice:show"
+  },
+  onRender() {
+    // MathJax.Hub.Queue(["Typeset",MathJax.Hub,this.$el[0]])
+  }
+});
+
+const ExercicesCollectionView = CollectionView.extend({
+  className: "list-group",
+  emptyView: NoExerciceView,
+  childView: ExerciceItemView,
+  childViewEventPrefix: "item",
+  behaviors: [FilterList],
+  filterKeys: ["title", "description", "keyWords"]
+});
+
+export { ListLayout, ExercicesPanel, ExercicesCollectionView }
