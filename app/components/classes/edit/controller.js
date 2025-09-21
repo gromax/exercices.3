0f7ac531a7a45b336c1260b2ed62fd02d6bcf41e
gromax <gromaxx@gmail.com>
2025-09-21 23:39:11 +0200

    newClasseView

--- app/components/classes/edit/controller.js
+++ app/components/classes/edit/controller.js
@@ -1,5 +1,5 @@
 import { MnObject, Region } from 'backbone.marionette';
-import { EditClasseView } from './views.js';
+import { EditClasseView, NewClasseView } from './views.js';
 
 const Controller = MnObject.extend({
   channelName: "app",
@@ -18,12 +18,22 @@ const Controller = MnObject.extend({
     const view = new EditClasseView({
       model: classe,
       errorCode: "001",
+      title: `Modification de la classe ${classe.get("nom")}`
     });
 
     view.on("success", (model, resp) => {
       channel.trigger("classe:show", model.get("id"));
     });
     new Region({ el: '#main-region' }).show(view);
+  },
+
+  newClasseView(model) {
+    const newClasseView = new NewClasseView({
+      model: model,
+      errorCode: "002"
+    });
+    new Region({ el: '#modal' }).show(newClasseView);
+    return newClasseView;
   }
 });
 
