47190b99570c0146448056ee19e89fe115e2124d
gromax <gromaxx@gmail.com>
2025-09-22 22:18:39 +0200

    liens ordinaires à la place des events

--- app/components/home/show/admin_prof_panel.js
+++ app/components/home/show/admin_prof_panel.js
@@ -4,15 +4,6 @@ import admin_prof_tpl from '@templates/home/show/home-admin-prof.jst';
 const AdminProfPanel = View.extend({
   className: "jumbotron",
   template: admin_prof_tpl,
-  events: {
-    "click a.js-menu-item": "clickMenuItem"
-  },
-
-  clickMenuItem(e) {
-    e.preventDefault();
-    let cible = $(e.currentTarget).attr("cible");
-    this.trigger(`show:list`, cible);
-  },
 
   templateContext() {
     return {
--- app/components/home/show/off_view.js
+++ app/components/home/show/off_view.js
@@ -4,10 +4,6 @@ import off_tpl from '@templates/home/show/home-off.jst';
 const OffView = View.extend({
   className: "jumbotron",
   template: off_tpl,
-  triggers: {
-    "click a.js-login": "home:login",
-    "click a.js-join": "home:join",
-  },
   templateContext(){
     return {
       version: APP_VERSION
