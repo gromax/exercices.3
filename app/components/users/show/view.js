2d2bf74df90f783119fe223208d693de7acede62
gromax <gromaxx@gmail.com>
2025-09-22 22:21:28 +0200

    boutons deviennent simples liens, plus d'events donc

--- app/components/users/show/view.js
+++ app/components/users/show/view.js
@@ -4,10 +4,6 @@ import show_user_tpl from '@templates/users/show/show-user.jst'
 
 const ShowUserView = View.extend ({
   template: show_user_tpl,
-  triggers: {
-    "click a.js-edit": "click:edit",
-    "click a.js-editPwd": "click:edit:pwd"
-  }
 });
 
 export { ShowUserView };
