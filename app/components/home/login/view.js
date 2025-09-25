c9c8ce1f38609d7719280b45ef6ece024f74ca6e
gromax <gromaxx@gmail.com>
2025-09-25 22:37:07 +0200

    login view rendue plus standard et beaucoup plus simple

--- app/components/home/login/view.js
+++ app/components/home/login/view.js
@@ -1,40 +1,17 @@
 import { View } from 'backbone.marionette';
-import { SubmitClicked } from '../../behaviors.js';
+import {Form} from '../../behaviors.js';
 import login_tpl from '@templates/home/login/home-login.jst';
 
 const LoginView = View.extend({
   className:"card",
   template: login_tpl,
-  behaviors: [{
-    behaviorClass: SubmitClicked,
-    messagesDiv: "messages"
-  }],
-  showForgotten: false, // défault value
-  generateTitle: true,
-  events: {
-    "click button.js-forgotten": "forgottenClicked"
-  },
-  initialize() {
-    this.title = this.options.title || "Connexion";
-  },
-  onRender() {
-    if (this.getOption("generateTitle")){
-      let $title = $("<div>", { text: "Connexion", class:"card-header"});
-      this.$el.prepend($title);
-    }
-  },
+  behaviors: [Form],
+  showForgotten: false, // default value
   templateContext() {
     return {
       showForgotten: this.getOption("showForgotten")
     };
   },
-  forgottenClicked(e) {
-    e.preventDefault();
-    const form = this.$el.querySelector('form');
-    const fdata = new FormData(form);
-    const data = Object.fromEntries(fdata.entries());
-    this.trigger("login:forgotten", data.identifiant);
-  }
 });
 
 export { LoginView }
