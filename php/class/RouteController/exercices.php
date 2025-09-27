13cfe23039ae9952449231923c2d4bbdc220c610
gromax <gromaxx@gmail.com>
2025-09-27 20:21:05 +0200

    gestion des routes edit et new

--- php/class/RouteController/exercices.php
+++ php/class/RouteController/exercices.php
@@ -6,38 +6,147 @@ use BDDObject\Exercice;
 
 class exercices
 {
-    /**
-     * paramères de la requète
-     * @array
-     */
-    private $params;
-    /**
-     * Constructeur
-     */
-    public function __construct($params)
-    {
-        $this->params = $params;
-    }
+  /**
+   * paramères de la requète
+   * @array
+   */
+  private $params;
+  /**
+   * Constructeur
+   */
+  public function __construct($params)
+  {
+    $this->params = $params;
+  }
 
-    public function fetch()
+  public function insert()
+  {
+    $uLog=Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
     {
-        $id = (integer) $this->params['id'];
-        $exercice = Exercice::getObject($id);
-        if ($exercice===null)
-        {
-            EC::set_error_code(404);
-            return false;
-        }
-        else
-        {
-            return $exercice->toArray();
-        }
+      EC::set_error_code(401);
+      return false;
     }
+    if ($uLog->isEleve()) {
+      // interdit pour élève
+      EC::set_error_code(403);
+      return false;
+    }
+    $data = json_decode(file_get_contents("php://input"),true);
+    $data["idOwner"] = $uLog->getId();
+    $exercice = new Exercice($data);
+    $reponse = $exercice->insert();
 
-    public function fetchList()
+    if ($reponse === null)
     {
-
-        return Exercice::getList();
+      EC::set_error_code(501);
+      return false;
     }
+    if (is_array($reponse))
+    {
+      // erreurs de validation
+      EC::set_error_code(422);
+      return $reponse;
+    }
+    return $exercice->toArray();
+  }
+
+  public function update()
+  {
+    $uLog=Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
+    {
+      EC::set_error_code(401);
+      return false;
+    }
+    if ($uLog->isEleve()) {
+      // Interdit pour élève
+      EC::set_error_code(403);
+      return false;
+    }
+    $id = (integer) $this->params['id'];
+    $exercice = Exercice::getObject($id);
+    if ($exercice === null)
+    {
+      EC::set_error_code(404);
+      return false;
+    }
+    if (!$uLog->isAdmin() && $exercice->getKey("idOwner") !== $uLog->getId())
+    {
+      // Interdit, pas propriétaire ni admin
+      EC::set_error_code(403);
+      return false;
+    }
+    $data = json_decode(file_get_contents("php://input"),true);
+    $reponse = $exercice->update($data);
+    if (is_array($reponse))
+    {
+      // erreurs de validation
+      EC::set_error_code(422);
+      return $reponse;
+    }
+    if ($reponse === false)
+    {
+      EC::set_error_code(501);
+      return false;
+    }
+    return $exercice->toArray();
+  }
+
+
+  public function fetch()
+  {
+    $id = (integer) $this->params['id'];
+    $exercice = Exercice::getObject($id);
+    if ($exercice===null)
+    {
+      EC::set_error_code(404);
+      return false;
+    }
+    else
+    {
+      return $exercice->toArray();
+    }
+  }
+
+  public function fetchList()
+  {
+    return Exercice::getList();
+  }
+
+  public function delete()
+  {
+    $uLog=Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
+    {
+      EC::set_error_code(401);
+      return false;
+    }
+    if ($uLog->isEleve()) {
+      // interdit pour élève
+      EC::set_error_code(403);
+      return false;
+    }
+    $id = (integer) $this->params['id'];
+    $exercice = Exercice::getObject($id);
+    if ($exercice === null)
+    {
+      EC::set_error_code(404);
+      return false;
+    }
+    if (!$uLog->isAdmin() && $exercice->getKey("idOwner") !== $uLog->getId())
+    {
+      // Interdit, pas propriétaire ni admin
+      EC::set_error_code(403);
+      return false;
+    }
+    if (!$exercice->delete())
+    {
+        EC::set_error_code(501);
+        return false;
+    }
+    EC::add_message("Model successfully destroyed!");
+    return true;
+  }
 }
 ?>
