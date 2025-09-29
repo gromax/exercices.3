1a0cd1c6131f448b6aa8b398170f0856bcb96042
gromax <gromaxx@gmail.com>
2025-09-29 11:57:17 +0200

    ajout gestion d'une pile d'exécution

--- app/tools/mymath.js
+++ app/tools/mymath.js
@@ -1,5 +1,9 @@
-import { Parser } from './parser/parser.js';
+import Parser from './parser/parser.js';
 import { build } from './parser/rpnbuilder.js';
+import Alea from './misc/alea.js';
+import Algebrite from 'algebrite';
+
+const MODULES = [Alea];
 
 /**
  * parse
@@ -10,19 +14,55 @@ function parse(input) {
     return build(parsed.rpn);
 }
 
-function substituteLabels(expr, params) {
-  return expr.replace(/@(\w+)/g, (match, label) => {
-    if (!params.hasOwnProperty(label)) {
-        throw new Error(`Lecture d'un paramètre inconnu : ${label}`);
-    }
-    return `(${params[label]})`;
-  });
+function executePile(pile) {
+  const operandes = [];
+  if (pile.length === 0) return null;
+  while (pile.length > 0) {
+      const top = pile.pop();
+      if (typeof top !== 'string') {
+          operandes.push(top);
+          continue;
+      }
+      if (!/^\w+\.\w+$/.test(top)) {
+        operandes.push(top);
+        continue;
+      }
+      const match = top.match(/^(\w+)\.(\w+)$/);
+      const moduleName = match[1];
+      const functionName = match[2];
+      const m = MODULES.find(mod => mod.name == moduleName);
+      if (!m) {
+        throw new Error(`Module ${moduleName} non trouvé`);
+      }
+      if (!m.hasOwnProperty(functionName)) {
+        throw new Error(`Fonction ${functionName} non trouvée dans le module ${moduleName}`);
+      }
+      const n = m[functionName].length;
+      if (operandes.length < n) {
+          throw new Error(`Pas assez d'opérandes pour l'opération ${top}`);
+      }
+      const args = operandes.splice(operandes.length - n, n).reverse();
+      const result = m[functionName](...args);
+      if (result !== undefined) {
+          operandes.push(result);
+      }
+  }
+  if (operandes.length !== 1) {
+      throw new Error("La pile n'a pas été réduite à une seule valeur.");
+  }
+  return operandes[0];
 }
 
-function evaluate(expression, params) {
-    const substituted = substituteLabels(expression, params);
-    const expr = parse(substituted);
-    return String(expr.toDecimal());
+function evaluate(expression) {
+    let expr = expression.trim();
+    if (/^\[.*\]$/.test(expr)) {
+      // expression commence par [ et finit par ]
+      let pile = expr.slice(1, -1).trim().split(',').map(
+        s => s.trim()
+      );
+      return String(executePile(pile));
+    }
+    return String(Algebrite.simplify(expr));
 }
 
 const MyMath = {
