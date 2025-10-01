d8cfe281db693127f217f819280f4ed2651247b3
gromax <gromaxx@gmail.com>
2025-10-01 20:19:53 +0200

    ajout d'un bouton temporaire pour le debug

--- app/components/exercices/edit/views.js
+++ app/components/exercices/edit/views.js
@@ -3,12 +3,27 @@ import { Form } from '../../behaviors.js';
 import UseBootstrapTag from 'use-bootstrap-tag'
 import exercice_edit_tpl from '@templates/exercices/edit/exercice-edit.jst'
 
+// test des modules
+//import Algebrite from 'algebrite';
+import Tools from '../tools.js';
+
+
 const EditExerciceView = View.extend({
   template: exercice_edit_tpl,
   behaviors: [Form],
+  triggers: {
+    "click .js-tempo": "form:tempo"
+  },
   onRender() {
     // Initialisation du composant de gestion des tags pour les mots-clés
     UseBootstrapTag(this.el.querySelector('#exercice-keywords'));
+  },
+  onFormTempo() {
+    this.el.querySelector("form").addEventListener("submit", (e) => {
+      const com = this.el.querySelector("#exercice-init").value;
+      const obj = Tools.initExoParams(com);
+      console.log(obj);
+    });
   }
 });
 
