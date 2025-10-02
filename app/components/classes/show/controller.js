d2b3a83b761bf9865875e60ca24da91410d49a6e
gromax <gromaxx@gmail.com>
2025-10-02 18:44:35 +0200

    simplification

--- app/components/classes/show/controller.js
+++ app/components/classes/show/controller.js
@@ -5,17 +5,21 @@ const Controller = MnObject.extend({
   channelName: 'app',
   show(id, classe) {
     const channel = this.getChannel();
+
     if (classe === undefined) {
-      channel.trigger("ariane:add", { text: "Classe inconnue", e: "classe:show", data: id, link: `classe:${id}` });
+      channel.trigger("ariane:reset", [
+        { text: "Classes", link: "classes" },
+        { text: "Classe inconnue", link: `classe:${id}` }
+      ]);
       channel.trigger("missing:item");
       return;
     }
-    channel.trigger("ariane:add", { text: classe.get("nom"), e: "classe:show", data: id, link: `classe:${id}` });
+    channel.trigger("ariane:reset", [
+      { text: "Classes", link: "classes" },
+      { text: classe.get("nom"), link: `classe:${id}` }
+    ]);
     const view = new ShowClasseView({
       model: classe
-    });
-    view.on("classe:edit", (item) => {
-      channel.trigger("classe:edit", item.get("id"));
     });
     new Region({ el: '#main-region' }).show(view);
   }
