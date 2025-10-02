7cf022e0498e2b63c5618bd793bf85fd24110df5
gromax <gromaxx@gmail.com>
2025-10-02 18:48:28 +0200

    standardisation de la gestion des classes, en accord avec Classe devenant héritier de Item

--- php/class/RouteController/classes.php
+++ php/class/RouteController/classes.php
@@ -8,367 +8,280 @@ use BDDObject\Logged;
 
 class classes
 {
-    /**
-     * paramères de la requète
-     * @array
-     */
-    private $params;
-    /**
-     * Constructeur
-     */
-    public function __construct($params)
+  /**
+   * paramères de la requète
+   * @array
+   */
+  private $params;
+  /**
+   * Constructeur
+   */
+  public function __construct($params)
+  {
+    $this->params = $params;
+  }
+  /**
+   * renvoie les infos sur l'objet d'identifiant id
+   * @return array
+   */
+  public function fetch()
+  {
+    $uLog =Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
     {
-        $this->params = $params;
+      EC::set_error_code(401);
+      return false;
     }
-    /**
-     * renvoie les infos sur l'objet d'identifiant id
-     * @return array
-     */
-    public function fetch()
+
+    if (isset($this->params['id']))
     {
-        $uLog =Logged::getConnectedUser();
-        if (!$uLog->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
+      $id = (integer) $this->params['id'];
+      return $this->fetchClasse($id);
+    }
 
-        if (isset($this->params['id']))
-        {
-            $id = (integer) $this->params['id'];
-            return $this->fetchClasse($id);
-        }
+    if ($uLog->isAdmin()) return Classe::getList();
+    if ($uLog->isProf()) return Classe::getList([
+        'wheres' => ['idOwner'=> $uLog->getId()]
+    ]);
+    if ($uLog->isEleve()) return Classe::getClassesList(['forEleve'=> $uLog->getId()]);
+    EC::set_error_code(403);
+    return false;
+  }
 
-        if ($uLog->isAdmin()) return Classe::getList();
-        if ($uLog->isProf()) return Classe::getList(array('ownerIs'=> $uLog->getId() ));
-        if ($uLog->isEleve()) return Classe::getList(array('forEleve'=> $uLog->getId() ));
-        EC::set_error_code(403);
+  private function fetchClasse($id) {
+    $uLog =Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
+    {
+      EC::set_error_code(401);
+      return false;
+    }
+    $classe = Classe::getObject($id);
+    if ($classe===null)
+    {
+      EC::set_error_code(404);
+      return false;
+    }
+    if ( $uLog->isAdmin() || $classe->get("idOwner") === $uLog->getId() )
+    {
+      return $classe->toArray();
+    }
+    EC::set_error_code(403);
+    return false;
+  }
+
+  public function fetchToJoin()
+  {
+    // Renvoie la liste des classes ouvertes à l'inscription
+    return Classe::getList([
+        "wheres" => ['ouverte' => true],
+        "hideCols" => ['pwd']
+    ]);
+  }
+
+  public function delete()
+  {
+    $uLog=Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
+    {
+      EC::addError("Utilisateur non connecté.");
+      EC::set_error_code(401);
+      return false;
+    }
+    elseif ($uLog->isEleve())
+    {
+      EC::addError("Interdit aux élèves.");
+      EC::set_error_code(403);
+      return false;
+    }
+    $id = (integer) $this->params['id'];
+    $classe=Classe::getObject($id);
+    if ($classe === null)
+    {
+      EC::addError("Classe introuvable.");
+      EC::set_error_code(404);
+      return false;
+    }
+    if(!$uLog->isAdmin() && $classe->get("idOwner") !== $uLog->getId())
+    {
+      EC::addError("Pas propriétaire de la classe.");
+      EC::set_error_code(403);
+      return false;
+    }
+    if ($classe->delete()) {
+      return array( "message" => "Model successfully destroyed!");
+    }
+    EC::set_error_code(501);
+    return false;
+  }
+
+  public function insert()
+  {
+    $uLog=Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
+    {
+      EC::set_error_code(401);
+      return false;
+    }
+    if ($uLog->isEleve()) {
+      // interdit pour élève
+      EC::set_error_code(403);
+      return false;
+    }
+    $data = json_decode(file_get_contents("php://input"),true);
+    if ($uLog->isAdmin() && isset($data["idOwner"]))
+    {
+      // Dans ce cas, l'utilisateur a la possibilité de créer une classe pour un autre
+      $idOwner = $data["idOwner"];
+      $owner =User::getObject($idOwner);
+      if (($owner == null) || !$owner->isProf())
+      {
+        EC::set_error_code(501);
         return false;
+      }
+      $data["idOwner"] = $owner;
+    } else {
+      // Sinon le propriétaire est forcément celui qui est connecté
+      $data["idOwner"] = $uLog->getId();
     }
+    $classe = new Classe($data);
+    $reponse = $classe->insert();
 
-    private function fetchClasse($id) {
-        $uLog =Logged::getConnectedUser();
-        if (!$uLog->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-        $classe = Classe::getObject($id);
-        if ($classe===null)
-        {
-            EC::set_error_code(404);
-            return false;
-        }
-        if ( $uLog->isAdmin() || $classe->isOwnedBy($uLog) )
-        {
-            return $classe->toArray();
-        }
-        EC::set_error_code(403);
-        return false;
-    }
-
-    public function fetchToJoin()
+    if ($reponse === null)
     {
-        // Renvoie la liste des classes ouvertes à l'inscription
-        return Classe::getList(array('forJoin'=> true ));
-    }
-
-    public function delete()
-    {
-        $uLog=Logged::getConnectedUser();
-        if (!$uLog->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-        elseif ($uLog->isEleve())
-        {
-            EC::set_error_code(403);
-            return false;
-        }
-        else
-        {
-            $id = (integer) $this->params['id'];
-            $classe=Classe::getObject($id);
-            if ($classe === null)
-            {
-                EC::set_error_code(404);
-                return false;
-            }
-            elseif($uLog->isAdmin() || $classe->isOwnedBy($uLog))
-            {
-                if ($classe->delete())
-                    return array( "message" => "Model successfully destroyed!");
-            }
-            else
-            {
-                EC::set_error_code(403);
-                return false;
-            }
-        }
         EC::set_error_code(501);
         return false;
     }
-
-    public function insert()
+    if (is_array($reponse))
     {
-        $uLog=Logged::getConnectedUser();
-        if (!$uLog->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-        if ($uLog->isEleve()) {
-            // interdit pour élève
-            EC::set_error_code(403);
-            return false;
-        }
-        $data = json_decode(file_get_contents("php://input"),true);
-        if ($uLog->isAdmin() && isset($data["idOwner"]))
-        {
-            // Dans ce cas, l'utilisateur a la possibilité de créer une classe pour un autre
-            $idOwner = $data["idOwner"];
-            $owner =User::getObject($idOwner);
-            if (($owner == null) || !$owner->isProf())
-            {
-                EC::set_error_code(501);
-                return false;
-            }
-            $data["owner"] = $owner;
-        } else {
-            // Sinon le propriétaire est forcément celui qui est connecté
-            $data["owner"] = $uLog;
-        }
+        // erreurs de validation
+        EC::set_error_code(422);
+        return $reponse;
+    }
+    return $classe->toArray();
+  }
 
-        $classe = new Classe($data);
-        $validation = $classe->insertion_update_validation();
-        if($validation === true)
-        {
-            $id = $classe->insertion();
-            if ($id!==null)
-            {
-                return $classe->toArray();
-            }
-        }
-        else
-        {
-            EC::set_error_code(422);
-            return array('errors'=>$validation);
-        }
-        // Si on en arrive là, erreur
-        EC::set_error_code(501);
-        return false;
+  public function update()
+  {
+    $uLog=Logged::getConnectedUser();
+    if (!$uLog->connexionOk())
+    {
+      EC::set_error_code(401);
+      return false;
+    }
+    if ($uLog->isEleve()) {
+      // Interdit pour élève
+      EC::set_error_code(403);
+      return false;
+    }
+    $id = (integer) $this->params['id'];
+    $classe = Classe::getObject($id);
+    if ($classe === null)
+    {
+      EC::set_error_code(404);
+      return false;
+    }
+    if (!$uLog->isAdmin() && ($classe->get("idOwner") !== $uLog->getId()))
+    {
+      // Interdit, pas propriétaire ni admin
+      EC::addError("Tentative de mise à jour d'une classe sans autorisation.");
+      EC::set_error_code(403);
+      return false;
+    }
+    $data = json_decode(file_get_contents("php://input"),true);
+    $reponse = $classe->update($data);
+    if (is_array($reponse))
+    {
+      // erreurs de validation
+      EC::set_error_code(422);
+      return $reponse;
+    }
+    if ($reponse === false)
+    {
+      EC::set_error_code(501);
+      return false;
+    }
+    return $classe->toArray();
+  }
+
+  public function join()
+  {
+    $idClasse = (integer) $this->params['id'];
+    $classe = Classe::getObject($idClasse);
+    if ($classe === null)
+    {
+      EC::set_error_code(404);
+      return false;
+    }
+    if (!$classe->get("ouverte"))
+    {
+      EC::addError("Classe fermée.");
+      EC::set_error_code(403);
+      return false;
     }
 
-    public function update()
+    $data = json_decode(file_get_contents("php://input"),true);
+    if (isset($data["pwdClasse"]))
     {
-        $uLog=Logged::getConnectedUser();
-        if (!$uLog->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-        if ($uLog->isEleve()) {
-            // Interdit pour élève
-            EC::set_error_code(403);
-            return false;
-        }
-        $id = (integer) $this->params['id'];
-        $classe=Classe::getObject($id);
-        if ($classe === null)
-        {
-            EC::set_error_code(404);
-            return false;
-        }
-        if ($uLog->isAdmin() || $classe->isOwnedBy($uLog))
-        {
-            $data = json_decode(file_get_contents("php://input"),true);
-            $validation = $classe->insertion_update_validation($data);
-            if ($validation===true)
-            {
-                $modOk = $classe->update($data);
-                if ($modOk === true)
-                {
-                    return $classe->toArray();
-                }
-            }
-            else
-            {
-                EC::set_error_code(422);
-                return array('errors' => $validation);
-            }
-        }
-        else
-        {
-            // Interdit, pas propriétaire ni admin
-            EC::set_error_code(403);
-            return false;
-        }
-        EC::set_error_code(501);
-        return false;
+      $pwdClasse = $data["pwdClasse"];
+    }
+    else
+    {
+      $pwdClasse = "";
     }
 
-    public function join()
+    if (!$classe->testPwd($pwdClasse))
     {
-        $idClasse = (integer) $this->params['id'];
-        $classe = Classe::getObject($idClasse);
-        if ($classe === null)
-        {
-            EC::set_error_code(404);
-            return false;
-        }
-        if (!$classe->isOpen())
-        {
-            EC::addError("Classe fermée.");
-            EC::set_error_code(403);
-            return false;
-        }
-
-        $data = json_decode(file_get_contents("php://input"),true);
-        if (isset($data["pwdClasse"]))
-        {
-            $pwdClasse = $data["pwdClasse"];
-        }
-        else
-        {
-            $pwdClasse = "";
-        }
-
-        if (!$classe->testPwd($pwdClasse))
-        {
-            EC::addError("Mot de passe invalide.");
-            EC::set_error_code(422);
-            return false;
-        }
-
-        // On procède à l'inscription
-        $data['idClasse'] = $idClasse;
-        $data['rank'] = User::RANK_ELEVE;
-        $user=new User($data);
-        $validation = $user->insertion_validation();
-        if ($validation === true)
-        {
-            $id = $user->insertion();
-            if ($id!==null)
-            {
-                return $user->toArray();
-            }
-        }
-        else
-        {
-            EC::set_error_code(422);
-            return array('errors'=>$validation);
-        }
-        EC::set_error_code(501);
-        return false;
+      EC::addError("Mot de passe invalide.");
+      EC::set_error_code(422);
+      return false;
     }
 
-    public function testMDP()
+    // On procède à l'inscription
+    $data['idClasse'] = $idClasse;
+    $data['rank'] = User::RANK_ELEVE;
+    $user=new User($data);
+    $validation = $user->insertion_validation();
+    if ($validation === true)
     {
-        $idClasse = (integer) $this->params['id'];
-        $pwd = "";
-        if (isset($_GET['pwd']))
-        {
-            $pwd = $_GET['pwd'];
-        }
+      $id = $user->insertion();
+      if ($id!==null)
+      {
+        return $user->toArray();
+      }
+    }
+    else
+    {
+      EC::set_error_code(422);
+      return array('errors'=>$validation);
+    }
+    EC::set_error_code(501);
+    return false;
+  }
 
-        $classe = Classe::getObject($idClasse);
-
-        if ($classe === null)
-        {
-            EC::addError("La classe n'existe pas");
-            EC::set_error_code(404);
-            return false;
-        }
-        if (!$classe->testPwd($pwd))
-        {
-            EC::addError("Mot de passe invalide.");
-            EC::set_error_code(422);
-            return false;
-        }
-        return array("message"=>"Mot de passe correct");
+  public function testMDP()
+  {
+    $idClasse = (integer) $this->params['id'];
+    $pwd = "";
+    if (isset($_GET['pwd']))
+    {
+      $pwd = $_GET['pwd'];
     }
 
-    public function fill()
+    $classe = Classe::getObject($idClasse);
+
+    if ($classe === null)
     {
-        $uLog=Logged::getConnectedUser();
-        if (!$uLog->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-
-        if ($uLog->isAdmin())
-        {
-            $idClasse = (integer) $this->params['id'];
-            $classe = Classe::getObject($idClasse);
-            if ($classe === null)
-            {
-                EC::set_error_code(404);
-                return false;
-            }
-            $messages = array();
-            $inserteds = array();
-            $data = $_POST; //json_decode(file_get_contents("php://input"),true);
-            if (isset($data["liste"])) { $liste= $data["liste"]; } else { $liste=""; }
-            $arr_liste = explode(PHP_EOL, $liste);
-            foreach ($arr_liste as $item) {
-                $item = trim($item);
-                if ($item!=="")
-                {
-                    $arr_item = explode(";",$item);
-                    if (count($arr_item)==3) {
-                        $arr_item[] = "";
-                    }
-                    if (count($arr_item)<4) {
-                        $messages[] = "$item => mal formatté";
-                    } else if (($arr_item[2]=="")&&($arr_item[3]=="")){
-                        $messages[] = "$item => cas et email vides";
-                    } else {
-                        $itData = array("nom"=>$arr_item[0], "prenom"=>$arr_item[1], "cas"=>$arr_item[2], "email"=>$arr_item[3], "pwd"=>"dfge_sx".rand(1000,9999), "idClasse"=>$idClasse, "rank"=>User::RANK_ELEVE);
-                        $user=new User($itData);
-                        $user->casToEmail(); // crée un email sur le cas si c'est possible et l'email n'est pas défini
-                        $validation = $user->insertion_validation();
-                        if ($validation === true)
-                        {
-                            $id = $user->insertion();
-                            if ($id!==null)
-                            {
-                                $inserteds[] = $user->toArray();
-                            }
-                            else
-                            {
-                                $messages[] = "$item => Échec d'insertion";
-                            }
-                        }
-                        else
-                        {
-                            $errorMessage = array();
-                            foreach ($validation as $k => $v) {
-                                if (is_array($v)) {
-                                    $errorMessage[] = "($k) ".implode(" & ",$v);
-                                } else {
-                                    $errorMessage[] = "($k) $v";
-                                }
-                            }
-                            $messages[] = "$item => Invalide [".implode("+", $errorMessage)."]";
-                        }
-                    }
-                }
-            }
-
-
-            return array("inserteds"=>$inserteds, "message"=>$messages);
-        }
-        else
-        {
-            // Interdit, pas admin
-            EC::set_error_code(403);
-            return false;
-        }
-
-
+      EC::addError("La classe n'existe pas");
+      EC::set_error_code(404);
+      return false;
     }
-
+    if (!$classe->testPwd($pwd))
+    {
+      EC::addError("Mot de passe invalide.");
+      EC::set_error_code(422);
+      return false;
+    }
+    return array("message"=>"Mot de passe correct");
+  }
 }
 ?>
