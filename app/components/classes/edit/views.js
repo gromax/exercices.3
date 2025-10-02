d927c42876ccc6ecd2e879550d28dc0a2e3c3dcc
gromax <gromaxx@gmail.com>
2025-10-02 18:44:21 +0200

    suppression de ce qui concerne fill

--- app/components/classes/edit/views.js
+++ app/components/classes/edit/views.js
@@ -1,7 +1,6 @@
 import { View } from 'backbone.marionette'
 import { Form } from '../../behaviors.js'
 import edit_tpl from '@templates/classes/edit/classe-form-edit.jst'
-import fill_tpl from '@templates/classes/list/classe-fill-form.jst'
 
 const EditClasseView = View.extend({
   template: edit_tpl,
@@ -14,13 +13,4 @@ const EditClasseView = View.extend({
   }
 });
 
-/*const FillClasseView = View.extend({
-  template: fill_tpl,
-  behaviors: [Form],
-  initialize: function() {
-    this.title = "Nouvelle classe pour " + this.getOption('nomProf');
-  }
-
-});*/
-
-export { EditClasseView } //, FillClasseView }
+export { EditClasseView }
