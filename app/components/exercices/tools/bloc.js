bec742e2c647dddde70258e4b5c684c7ecb5eb35
gromax <gromaxx@gmail.com>
2025-10-04 19:23:12 +0200

    début de la prise en charge d'un bloc de rendu

--- app/components/exercices/tools/bloc.js
+++ app/components/exercices/tools/bloc.js
@@ -1,4 +1,11 @@
+/**
+ * L'objectif de cette classe est de gérer des blocs
+ * de rendu identifiés par une balise de type <label param1 param2 ...>
+ */
+
 import BlocParent from './blocparent.js';
+import Affectation from './affectation.js';
+import IfBloc from './ifbloc.js';
 
 class Bloc extends BlocParent {
     closed = false;
@@ -25,6 +32,53 @@ class Bloc extends BlocParent {
         this.label = label;
         this.params = paramsString;
         this.closed = closed;
+        this.evaluationResult = null;
+    }
+
+    /**
+     * Exécute les morceaux de code du bloc
+     * et effectue les substitutions de texte nécessaire
+     * de façon à obtenir un bloc de texte final qui pourra
+     * être rendu.
+     * @param {Object} params 
+     */
+    evaluate(params, options) {
+        let program = [...this.children];
+        let i = 0;
+        while (i<program.length) {
+            let item = program[i];
+            if (typeof item === 'string') {
+                program[i] = BlocParent.substituteLabels(item, {...params, ...options});
+                i++;
+                continue;
+            }
+            if (item instanceof Affectation) {
+                item.doAffectation(params, ...options);
+                program.splice(i, 1);
+                continue;
+            }
+            if (item instanceof IfBloc) {
+                const result = item.evaluate(params, options);
+                if (item.type === 'needed') {
+                    if (!result) {
+                        throw Error("Condition 'needed' non satisfaite");
+                    }
+                    program.splice(i, 1);
+                    continue;
+                }
+                const ifChildren = result ? item.children : item.elseChildren;
+                program.splice(i + 1, 0, ...ifChildren);
+                program.splice(i, 1);
+                continue;
+            }
+            if (item instanceof Bloc) {
+                item.evaluate(params, options);
+                i++;
+                continue;
+            }
+            throw new Error("Instruction inconnue dans un bloc : " + item);
+        }
+        this.evaluationResult = program;
     }
 
 
