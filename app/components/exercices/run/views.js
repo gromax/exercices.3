aa0736f3f50c7bdbd62c3d1ddcd2cb4032785024
gromax <gromaxx@gmail.com>
2025-10-08 23:33:35 +0200

    nouvelles vues, notamment pour bloc text

--- app/components/exercices/run/views.js
+++ app/components/exercices/run/views.js
@@ -1,5 +1,7 @@
 import { View } from 'backbone.marionette';
 import options_tpl from '@templates/exercices/run/exercice-options.jst'
+import text_tpl from '@templates/exercices/run/exercice-text.jst'
+import params_tpl from '@templates/exercices/run/exercice-apercu-params.jst' // pour l'aperçu des paramètres
 
 const OptionsView = View.extend({
   template: options_tpl,
@@ -21,4 +23,28 @@ const OptionsView = View.extend({
   }
 });
 
-export { OptionsView };
+const ParamsView = View.extend({
+  template: params_tpl,
+  templateContext() {
+    return {
+      params: this.getOption("params")
+    };
+  }
+});
+
+
+const TextView = View.extend({
+  template: text_tpl,
+  templateContext() {
+    return {
+      header: this.getOption("params")?.header||false,
+      subtitle: this.getOption("params")?.subtitle||false,
+      paragraphs: this.getOption("content"),
+      footer: this.getOption("footer")||false,
+      info: this.getOption("params")?.info||false,
+      warning: this.getOption("params")?.warning||false
+    };
+  }
+});
+
+export { OptionsView, ParamsView, TextView };
