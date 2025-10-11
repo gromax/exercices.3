24ad60be73bed829c134072872191524ac2fdcaa
gromax <gromaxx@gmail.com>
2025-10-11 18:17:22 +0200

    maintenant fonctionnel

--- app/components/exercices/tools/FormBloc.js
+++ app/components/exercices/tools/FormBloc.js
@@ -1,34 +1,21 @@
 import Bloc from "./bloc";
+import { FormView } from "../run/views";
 
 class FormBloc extends Bloc {
     static LABELS = ['form', 'formulaire'];
-    static FIELDS = ['input', 'radio'];
-    constructor(label, paramsString, closed) {
-        super(label, paramsString, closed);
-        this.setParam('type', label);
-        this.setParam('fields', []);
-    }
-
-    run(params, options={}) {
-        if (this._executionChildren) {
-            // déjà exécuté
-            return this;
+    _customView() {
+        const subViews = [];
+        for (const child of this._executionChildren) {
+            if (typeof child.view === "function") {
+                const subView = child.view();
+                subViews.push(subView);
+            }
         }
-        super.run(params, options);
-        // pour un bloc de formulaire ne conserve que les blocs de texte et les champs
-        this._executionChildren = this._executionChildren.filter(
-            item => (item instanceof TextBloc) || (FormBloc.FIELDS.includes(item.label))
-        );
-        this.setParam('fields', this._executionChildren.filter(item => item instanceof Bloc && item.label === 'field').map(item => item.params));
-        return this;
-    }
-
-    toView(params, options) {
-        const result = this.run(params, options);
-        return new FormView({
-            fields: this._params["fields"] || [],
-            content: result.content,
+        const formView = new FormView({
+            name: this.header,
+            subViews: subViews
         });
+        return formView;
     }
 }
 
