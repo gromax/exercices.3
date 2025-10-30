33efd065abfb57eb4d23ed4cb8dc7d209bcfac43
gromax <gromaxx@gmail.com>
2025-10-30 23:31:41 +0100

    affichage notes pour un élève

--- app/components/notes/list/controller.js
+++ app/components/notes/list/controller.js
@@ -13,7 +13,6 @@ const Controller = MnObject.extend({
     const notesView = new NotesCollectionView({
       ncols: 2,
       collection: collecNotes,
-      showId: true,
       showUser: true,
       showDevoir: false,
       showNomOwner: false,
@@ -26,7 +25,45 @@ const Controller = MnObject.extend({
       channel.trigger("notes:devoir:user:show", idDevoir, idUser);
     });
     new Region({ el: '#main-region' }).show(notesView);
+  },
+
+  showNotesListForEleve(idUser, eleve, collecNotes) {
+    // il manque clairement un panel
+    const channel = this.getChannel();
+    const logged = channel.request("logged:get");
+    
+    if (logged.id === idUser) {
+      // si utilisateur est user alors se sont ses notes et c'est le home
+      channel.trigger("ariane:reset", []);
+    } else {
+      channel.trigger("ariane:reset", [
+        { text: "users", link: "#users" },
+        { text: `Élève : ${eleve.get('nomComplet')} ${eleve.get('prenom')}`, link: `#user:${idUser}` },
+        { text: "Notes", link: `#user:${idUser}/notes` }
+      ]);
+    }
+    const notesView = new NotesCollectionView({
+      ncols: 4,
+      collection: collecNotes,
+      showUser: false,
+      showDevoir: true,
+      showNomOwner: false,
+      showTimeLeft: true
+    });
+    notesView.on("item:show", (childView) => {
+      const model = childView.model;
+      const idDevoir = model.get('idDevoir');
+      const idUser = model.get('idUser');
+      if (logged.id === idUser) {
+        // si utilisateur est user alors se sont ses notes et c'est le home
+        channel.trigger("notes:my:devoir", idDevoir);
+      } else {
+        channel.trigger("popup:alert", "À réfléchir. Pb de navigation ?");
+      }
+    });
+    new Region({ el: '#main-region' }).show(notesView);
   }
+
 });
 
 export const controller = new Controller();
