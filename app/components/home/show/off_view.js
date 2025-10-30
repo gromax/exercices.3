8128d4537d355a132edf3eaf278d3de61f5dfd37
gromax <gromaxx@gmail.com>
2025-10-30 23:30:19 +0100

    suppression jumbotron

--- app/components/home/show/off_view.js
+++ app/components/home/show/off_view.js
@@ -2,7 +2,6 @@ import { View } from 'backbone.marionette';
 import off_tpl from '@templates/home/show/home-off.jst';
 
 const OffView = View.extend({
-  className: "jumbotron",
   template: off_tpl,
   templateContext(){
     return {
