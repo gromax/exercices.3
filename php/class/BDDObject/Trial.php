7e95ddc9fb2e4e505f04383a4f4fcbe5c4cb834c
gromax <gromaxx@gmail.com>
2025-11-04 22:43:57 +0100

    init au lieu inputs + foncton pour vérifier droit + PREFIX_BDD qui avaient été oubliés

--- php/class/BDDObject/Trial.php
+++ php/class/BDDObject/Trial.php
@@ -5,7 +5,7 @@ use PDO;
 use PDOException;
 use ErrorController as EC;
 
-final class Trial extends Item
+class Trial extends Item
 {
   protected static $BDDName = "trials";
 
@@ -17,12 +17,43 @@ final class Trial extends Item
       'idExoDevoir' => ['def' => 0, 'type'=> 'int'],     // id de l'exercice associé
       'idUser' => ['def' => 0, 'type'=> 'int'],     // id de l'utilisateur
       'note' => ['def' => 0, 'type'=> 'int'],        // note de l'essai
-      'inputs' => ['def' => "", 'type'=> 'string'], // JSON des inputs intialisant l'exercice
+      'init' => ['def' => "", 'type'=> 'string'], // JSON des intialisations de l'exercice
       'answers' => ['def' => "", 'type'=> 'string'], // JSON des réponses de l'utilisateur
       'date' => ['def' => date('Y-m-d'), 'type'=> 'date'], // date de l'essai
       'finished' => ['def' => false, 'type'=> 'bool'] // si l'essai est terminé
     ] ;
   }
+
+  public static function insertAllowed($idExoDevoir, $idUser)
+  {
+    // autorisé si :
+    // user dans la classe du devoir
+    // devoir ouvert
+    $date = date('Y-m-d');
+    require_once BDD_CONFIG;
+    try {
+      $pdo=new PDO(BDD_DSN,BDD_USER,BDD_PASSWORD);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $stmt = $pdo->prepare("SELECT devoirs.id from ".PREFIX_BDD."devoirs  AS devoirs
+        JOIN ".PREFIX_BDD."users AS users ON users.idClasse = devoirs.idClasse
+        JOIN ".PREFIX_BDD."exodevoirs AS exodevoirs ON exodevoirs.idDevoir = devoirs.id
+        WHERE exodevoirs.id = :idExoDevoir AND users.id = :idUser
+        AND devoirs.dateDebut <= :date AND devoirs.dateFin >= :date");
+      $stmt->bindValue(":idExoDevoir", $idExoDevoir, PDO::PARAM_INT);
+      $stmt->bindValue(":idUser", $idUser, PDO::PARAM_INT);
+      $stmt->bindValue(":date", $date, PDO::PARAM_STR);
+      $stmt->execute();
+      if ($stmt->rowCount() == 0) {
+        return false;
+      }
+      return true;
+    } catch(PDOException $e) {
+      EC::addError($e->getMessage(), "Trial/onUpdateSuccess");
+      return false;
+    }
+  }
+
+  ##################################### METHODES D'INSTANCE #####################################
 
   protected function onUpdateSuccess()
   {
@@ -35,7 +66,7 @@ final class Trial extends Item
     try {
       $pdo=new PDO(BDD_DSN,BDD_USER,BDD_PASSWORD);
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $stmt = $pdo->prepare("UPDATE noteexos SET note = MAX(note,$note) WHERE idExoDevoir = :idExoDevoir AND idUser = :idUser");
+      $stmt = $pdo->prepare("UPDATE ".PREFIX_BDD."noteexos SET note = MAX(note,$note) WHERE idExoDevoir = :idExoDevoir AND idUser = :idUser");
       $stmt->bindValue(":idExoDevoir", $idExoDevoir, PDO::PARAM_INT);
       $stmt->bindValue(":idUser", $idUser, PDO::PARAM_INT);
       $stmt->execute();
@@ -57,7 +88,7 @@ final class Trial extends Item
     try {
       $pdo=new PDO(BDD_DSN,BDD_USER,BDD_PASSWORD);
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $stmt = $pdo->prepare("INSERT INTO noteexos (idExoDevoir, idUser, note, trials)
+      $stmt = $pdo->prepare("INSERT INTO ".PREFIX_BDD."noteexos (idExoDevoir, idUser, note, trials)
         VALUES (:idExoDevoir, :idUser, :note, 1)
         ON DUPLICATE KEY UPDATE note = GREATEST(note, VALUES(note)), trials = trials + 1");
       $stmt->bindValue(":idExoDevoir", $idExoDevoir, PDO::PARAM_INT);
@@ -87,7 +118,7 @@ final class Trial extends Item
       $stmMax->execute();
       $maxNote = (int) $stmMax->fetchColumn();
 
-      $stmt = $pdo->prepare("UPDATE noteexos SET note = :note, trials = trials - 1 WHERE idExoDevoir = :idExoDevoir AND idUser = :idUser");
+      $stmt = $pdo->prepare("UPDATE ".PREFIX_BDD."noteexos SET note = :note, trials = trials - 1 WHERE idExoDevoir = :idExoDevoir AND idUser = :idUser");
       $stmt->bindValue(":idExoDevoir", $idExoDevoir, PDO::PARAM_INT);
       $stmt->bindValue(":idUser", $idUser, PDO::PARAM_INT);
       $stmt->bindValue(":note", $maxNote, PDO::PARAM_INT);
