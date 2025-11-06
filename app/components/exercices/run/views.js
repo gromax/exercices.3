f6a60cac70688e2cc299a22ea1b110918df4b4e5
gromax <gromaxx@gmail.com>
2025-11-06 19:08:16 +0100

    ajout de la gestion des erreur de validation sur format incorrect

--- app/components/exercices/run/views.js
+++ app/components/exercices/run/views.js
@@ -141,11 +141,49 @@ const FormView = View.extend({
     }
     const errors = blocParent.validation(data);
     if (errors) {
-      this.trigger("validation:error", errors);
+      this.showErrors(errors);
       return;
     }
     this.trigger("validation:success", data);
-  }
+  },
+
+  clearForm() {
+    let $view = this.$el;
+    $(".is-invalid",$view).each(function() { $(this).removeClass("is-invalid"); });
+    $(".is-valid",$view).each(function() { $(this).removeClass("is-valid"); });
+    $view.find(".invalid-feedback.d-block").remove();
+  },
+
+  showErrors(errors) {
+    let $view = this.$el;
+    this.clearForm();
+
+    $view.find("input").each(function(index, item) {
+      $(item).addClass("is-valid").off("input").on("input", function() {
+        $(this).removeClass("is-invalid").removeClass("is-valid");
+      });
+    });
+    Object.entries(errors).forEach(([key, value]) => {
+      const $inp = $view.find(`input[name='${key}']`);
+      if ($inp.length === 0) {
+        console.warn(`Aucun champ trouvé pour la clé ${key}`);
+        return;
+      }
+      let $feedback = $inp.siblings('.invalid-feedback').first();
+      if ($feedback.length === 0) {
+        let $parent = $inp.parent();
+        $feedback = $("<div class='invalid-feedback d-block'></div>").appendTo($parent);
+      }
+      let html = value;
+      if (Array.isArray(value)){
+        let reduceFct = function(m,v,i) { return `${m}<p>${v}</p>`; }
+        html = _.reduce(value, reduceFct, "")
+      }
+      $feedback.html(html);
+      $inp.removeClass("is-valid");
+      $inp.addClass("is-invalid");
+    });
+  },
 });
 
 const ResultsView = View.extend({
