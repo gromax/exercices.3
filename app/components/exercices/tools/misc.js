f7b134147b4b23f6bd114d039611f16b5173b9be
gromax <gromaxx@gmail.com>
2025-11-06 19:11:05 +0100

    substitutelabels déplacé vers myMath

--- app/components/exercices/tools/misc.js
+++ app/components/exercices/tools/misc.js
@@ -1,27 +1,9 @@
 import MyMath from '@tools/mymath.js';
 
-/**
- * remplace les labels @label dans une expression par leur valeur
- * @param {string} expr une expression
- * @param {object} params les paramètres connus
- * @returns {string} une chaîne où les paramètres connus ont été remplacés par leur valeur
- */
-function substituteLabels(expr, params, forceParenthesis=false) {
-    return expr.replace(/@(\w+)/g, (match, label) => {
-        if (!params.hasOwnProperty(label)) {
-            return `@${label}`; // ne remplace pas si le paramètre n'existe pas
-        }
-        if (forceParenthesis) {
-            return `(${String(params[label])})`;
-        }
-        return String(params[label]);
-    });
-}
-
 function substituteExpressions(str, params) {
     return str.replace(/\{([^:]+):\s*([\w]*|\$)?\}/g, (match, expr, format) => {
-        const sexpr = substituteLabels(expr, params, true);
-        return MyMath.evaluate(sexpr, format);
+        const evaluation = MyMath.evaluate(expr, params, true);
+        return MyMath.evaluate(evaluation, format);
     });
 }
 
@@ -32,7 +14,6 @@ function substituteLatex(string) {
 }
 
 export {
-    substituteLabels,
     substituteLatex,
     substituteExpressions
 };
