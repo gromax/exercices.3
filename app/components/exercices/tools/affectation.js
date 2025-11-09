7008a0025c5b63de88ecc60dc3dd8122416cb283
gromax <gromaxx@gmail.com>
2025-11-09 11:33:35 +0100

    en plus de la simplification =, :=, j'ajoute la possibilité d'affecter un tableau en précisant un nombre devant le égal genre @x <:2>= ...

--- app/components/exercices/tools/affectation.js
+++ app/components/exercices/tools/affectation.js
@@ -2,45 +2,52 @@ import MyMath from '@tools/mymath.js';
 
 class Affectation {
     static parse(line) {
-        const regex = /^@([a-zA-Z_][a-zA-Z0-9_]*)\s*(:?=)\s*(.+)$/;
+        const regex = /^@([a-zA-Z_][a-zA-Z0-9_]*)\s*(<:(.+)>)?=\s*(.+)$/;
         const m = line.match(regex);
         if (!m) {
             return null;
         }
-        const [, tag, operator, value] = m;
-        return new Affectation(tag, operator, value);
+        const [, tag, , repeater, value] = m;
+        return new Affectation(tag, repeater, value);
     }
 
-    constructor(tag, operator, value) {
+    constructor(tag, repeater, value) {
         this._tag = tag;
-        this._operator = operator;
         this._value = value;
+        this._repeater = repeater;
     }
 
     /**
      * Réalise l'affectation dans params
      * protectedParams sont des paramètres protégés qui ne peuvent pas être modifiés
-     * saved est un tableau des paramètres précédemment définis
-     * il peuvent donc être écrasés seulement si on le force avec :=
      * @param {object} params 
      * @param {object} protectedParams 
-     * @param {array} saved 
      */
-    doAffectation(params, protectedParams, saved = []) {
-        if (saved.includes(this._tag) && (this._operator === '=')) {
-            // fonctionnement normal, on ignore l'affectation si déjà défini
-            // ignoré si déjà défini et opérateur =
-            return;
-        }
+    doAffectation(params, protectedParams) {
         if (this._tag in protectedParams) {
             // situation anormale, on ne peut pas écraser un paramètre protégé
             throw new Error(`Le paramètre ${this._tag} est protégé et ne peut pas être redéfini.`);
         }
-        params[this._tag] = MyMath.evaluate(this._value, { ...params, ...protectedParams });
+        if (this._repeater === undefined) {
+            params[this._tag] = MyMath.evaluate(this._value, { ...params, ...protectedParams });
+            return;
+        }
+        const n = Number(MyMath.evaluate(this._repeater, { ...params, ...protectedParams }));
+        if (!Number.isInteger(n) || n < 0) {
+            throw new Error(`La valeur de répétition pour le paramètre ${this._tag} doit être un entier positif.`);
+        }
+        const arr = [];
+        for (let i = 0; i < n; i++) {
+            arr.push(MyMath.evaluate(this._value, { ...params, ...protectedParams, "@index": i }));
+        }
+        params[this._tag] = arr;
     }
 
     toString() {
-        return `@${this._tag} ${this._operator} ${this._value}`;
+        if (this._repeater!==undefined) {
+            return `@${this._tag} <:${this._repeater}> = ${this._value}`;
+        }
+        return `@${this._tag} = ${this._value}`;
     }
 
     run(params, caller) {
