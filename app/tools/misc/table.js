e6a9c8d518ee0b3f62478a87ecaa4eff59133a95
gromax <gromaxx@gmail.com>
2025-11-10 20:28:39 +0100

    ajout module pour tableau

---
+++ app/tools/misc/table.js
@@ -0,0 +1,36 @@
+class Table {
+    static indice(val, arr) {
+        if (!Array.isArray(arr)) {
+            throw new Error(`Le second argument de Table.indice doit être un tableau.`);
+        }
+        return arr.indexOf(val);
+    }
+
+    static indices(val, arr) {
+        if (!Array.isArray(arr)) {
+            throw new Error(`Le second argument de Table.indices doit être un tableau.`);
+        }
+        return arr.reduce((acc, item, index) => {
+            if (String(item) === String(val)) {
+                acc.push(index);
+            }
+            return acc;
+        }, []);
+    }
+
+    static size(arr) {
+        if (!Array.isArray(arr)) {
+            throw new Error(`L'argument de Table.size doit être un tableau.`);
+        }
+        return arr.length;
+    }
+
+    static sum(arr) {
+        if (!Array.isArray(arr)) {
+            throw new Error(`L'argument de Table.sum doit être un tableau.`);
+        }
+        return arr.reduce((acc, val) => acc + val, 0);
+    }
+}
+
+export default Table;
