9ba3b83f3a5295687920554be65912eef2cc9279
gromax <gromaxx@gmail.com>
2025-11-11 15:46:19 +0100

    criterion inutile + options pour voir addbutton

--- app/components/exercices/list/controller.js
+++ app/components/exercices/list/controller.js
@@ -5,14 +5,17 @@ import renderMathInElement from "katex/contrib/auto-render";
 
 const Controller = MnObject.extend({
   channelName: "app",
-  makeView(exercices, criterion, region) {
+  makeView(exercices, region, options) {
+    options = options || {};
     const channel = this.getChannel();
-    criterion = criterion ? "" : "";
     const layout = new ListLayout();
-    const panel = new ExercicesPanel({ filterCriterion: criterion });
+    const panel = new ExercicesPanel({
+      filterCriterion: options.criterion || "",
+      showAddButton: options.showAddButton || false
+    });
     const listExercicesView = new ExercicesCollectionView({
       collection: exercices,
-      filterCriterion: criterion
+      filterCriterion: options.criterion || ""
     });
 
     panel.on("items:filter", (filterCriterion) => {
@@ -38,10 +41,12 @@ const Controller = MnObject.extend({
     return { listExercicesView, panel, layout };
   },
   
-  list(exercices, criterion) {
+  list(exercices) {
     const channel = this.getChannel();
+    const logged = channel.request("logged:get");
+    const showAddButton = logged.isProf() || logged.isAdmin();
     const region = channel.request("region:main");
-    const { listExercicesView } = this.makeView(exercices, criterion, region);
+    const { listExercicesView } = this.makeView(exercices, region, { showAddButton });
     listExercicesView.on("item:sujet:exercice:show", (childView) => {
       channel.trigger("sujet:exercice:show", childView.model.get("id"));
     });
