a1dad151e49a3c7f03748ce5ccdb8a2c8ab65621
gromax <gromaxx@gmail.com>
2025-11-11 21:50:07 +0100

    maj de API

--- app/assets/api.php
+++ app/assets/api.php
@@ -1,35 +1,33 @@
 <?php
 use ErrorController as EC;
 
-// Pour le dev
-// Afficher les erreurs à l'écran
-ini_set('display_errors', 1);
-// Enregistrer les erreurs dans un fichier de log
-ini_set('log_errors', 1);
-// Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
-ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
-// Afficher les erreurs et les avertissements
-error_reporting(E_ALL);
-
 require_once "../php/constantes.php";
+
+// Pour le dev
+if (DEV_MODE) ini_set('display_errors', 1); // afficher erreurs à l'écran
+ini_set('log_errors', 1); // enregistrer erreurs dans un fichier de log
+ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+if (DEV_MODE) error_reporting(E_ALL); // Afficher les erreurs et les avertissements
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
 require_once "../php/classAutoLoad.php";
 require_once "../php/routes.php";
-define("DEV_MODE", true);
 
 $response = $router->load();
 EC::header(); // Doit être en premier !
 if ($response === false) {
-	echo json_encode(array("ajaxMessages"=>EC::messages()));
+    echo json_encode(array("ajaxMessages"=>EC::messages()));
 } else {
-	if (isset($response["errors"]) && (count($response["errors"])==0)) {
-		unset($response["errors"]);
-	}/* else {
-		$messages = EC::messages();
-		if (count($messages)>0) {
-			$response["errors"] = $messages;
-		}
-	}*/
-	echo json_encode($response);
+    if (isset($response["errors"]) && (count($response["errors"])==0)) {
+        unset($response["errors"]);
+    }/* else {
+        $messages = EC::messages();
+        if (count($messages)>0) {
+            $response["errors"] = $messages;
+        }
+    }*/
+    echo json_encode($response);
 }
 
 ?>
