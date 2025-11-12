55dd047648939a7f4f8f971e82e8a3e06dc66c5f
gromax <gromaxx@gmail.com>
2025-11-12 22:30:07 +0100

    fonction pour obtenir une étiquette à partir du rang

--- app/components/common/ranks.js
+++ app/components/common/ranks.js
@@ -3,5 +3,19 @@ export default {
   ADMIN: 2,
   PROF: 1,
   ELEVE: 0,
-  DISCONNECTED: -1
+  DISCONNECTED: -1,
+  getLabel(rank) {
+    switch(rank) {
+      case this.ROOT:
+        return "Root";
+      case this.ADMIN:
+        return "Admin";
+      case this.PROF:
+        return "Prof";
+      case this.ELEVE:
+        return "Élève";
+      default :
+        return "Off";
+    }
+  }
 };
