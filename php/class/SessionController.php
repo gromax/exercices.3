98b59fe414068540d437dff747032bee78111d4d
gromax <gromaxx@gmail.com>
2025-11-12 19:38:34 +0100

    c'est Logged qui précise les données transmises au token

--- php/class/SessionController.php
+++ php/class/SessionController.php
@@ -10,22 +10,18 @@ final class SessionController
 
     ##################################### METHODES STATIQUES #####################################
 
-    public static function make_token($id, $email, $rank)
+    public static function make_token($data)
     {
         $payload = array(
             "iat" => time(),                   // Heure d'émission
             "exp" => time() + 3600,           // Expiration (1 heure)
-            "data" => array(               // Données associées au token
-                "id" => $id,
-                "email" => $email,
-                "rank" => $rank
-            )
+            "data" => $data                    // Données associées au token
         );
         require_once JWT_CONFIG;
         return JWT::encode($payload, SECRET_KEY, 'HS256');
     }
 
-    public static function verify_token()
+    public static function readToken()
     {
         require_once JWT_CONFIG;
         $headers = getallheaders();
