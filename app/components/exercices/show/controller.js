2a4160c1f8c2ab9c577b550eee092db57c48b0c4
gromax <gromaxx@gmail.com>
2025-11-13 19:10:49 +0100

    ajout d'une région pour afficher le contenu d'un aperçu de l'exercice

--- app/components/exercices/show/controller.js
+++ app/components/exercices/show/controller.js
@@ -1,18 +1,22 @@
-import { MnObject, Region } from 'backbone.marionette';
+import { MnObject } from 'backbone.marionette';
 import { ShowExerciceView } from './view.js';
 
 const Controller = MnObject.extend({
   channelName: 'app',
   show(exercice) {
     const channel = this.getChannel();
+    const logged = channel.request("logged:get");
     if (!exercice) {
       channel.trigger("popup:error", "Exercice inconnu.");
       return;
     }
     const view = new ShowExerciceView({
-      model: exercice
+      model: exercice,
+      showModButton: logged.id === exercice.get("idOwner") || logged.isAdmin()
     });
     channel.request("region:main").show(view);
+    const region = view.getRegion('exercice');
+    require('../run/controller.js').controller.showApercu(exercice, region);
   }
 });
 
--- app/components/exercices/show/view.js
+++ app/components/exercices/show/view.js
@@ -3,6 +3,14 @@ import show_item_tpl from '@templates/exercices/show/show-exercice.jst'
 
 const ShowExerciceView = View.extend({
   template: show_item_tpl,
+  templateContext() {
+    return {
+      showModButton: this.options.showModButton || false
+    }
+  },
+  regions: {
+    exercice: ".js-exercice",
+  }
 });
 
 export { ShowExerciceView }
