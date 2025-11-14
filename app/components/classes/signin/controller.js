311b30008433764cd90c76cba0bd30582c74f496
gromax <gromaxx@gmail.com>
2025-11-14 19:56:14 +0100

    signin ne prenait pas la région unique

--- app/components/classes/signin/controller.js
+++ app/components/classes/signin/controller.js
@@ -1,24 +1,24 @@
-import { MnObject, Region } from 'backbone.marionette';
+import { MnObject } from 'backbone.marionette';
 import { SigninClassesCollectionView } from './views.js';
 
 const Controller = MnObject.extend({
-  channelName: 'app',
+    channelName: 'app',
 
-  showSigninClasses(classes) {
-    const channel = this.getChannel();
-    const logged = channel.request("logged:get");
-    if (logged.isEleve()) {
-      const idClasseEleve = logged.get("idClasse");
-      classes = classes.filter( (c) => c.get("id") !== idClasseEleve );
-    }
-    const listClassesView = new SigninClassesCollectionView({
-      collection: classes
-    });
-    listClassesView.on("item:join", (childView) => {
-      channel.trigger("user:classe:signin", childView.model.get("id"));
-    });
-    new Region({ el: "#main-region" }).show(listClassesView);
-  },
+    showSigninClasses(classes) {
+        const channel = this.getChannel();
+        const logged = channel.request("logged:get");
+        if (logged.isEleve()) {
+            const idClasseEleve = logged.get("idClasse");
+            classes = classes.filter( (c) => c.get("id") !== idClasseEleve );
+        }
+        const listClassesView = new SigninClassesCollectionView({
+            collection: classes
+        });
+        listClassesView.on("item:join", (childView) => {
+            channel.trigger("user:classe:signin", childView.model.get("id"));
+        });
+        channel.request("region:main").show(listClassesView);
+    },
 });
 
 export const controller = new Controller();
