4d7350397a09d8e0632d9602ae6a7587bfc97f44
gromax <gromaxx@gmail.com>
2025-11-14 19:58:58 +0100

    ajout possibilité de cloner un devoir

--- app/components/devoirs/list/controller.js
+++ app/components/devoirs/list/controller.js
@@ -3,29 +3,44 @@ import { DevoirsPanel, DevoirsCollectionView, ListLayout } from './views.js'
 
 
 const Controller = MnObject.extend({
-  channelName: 'app',
+    channelName: 'app',
 
-  list(devoirs) {
-    const channel = this.getChannel();
-    const logged = channel.request("logged:get");
+    list(devoirs) {
+        const channel = this.getChannel();
+        const logged = channel.request("logged:get");
 
-    const devoirsListView = new DevoirsCollectionView({
-      collection: devoirs,
-      showNomOwner: logged.isAdmin(),
-    });
-    
-    const listItemsLayout = new ListLayout();
-    const panel = new DevoirsPanel({
-      showAddButton: !logged.isAdmin(),
-    });
+        const devoirsListView = new DevoirsCollectionView({
+            collection: devoirs,
+            showNomOwner: logged.isAdmin(),
+        });
+        
+        const listItemsLayout = new ListLayout();
+        const panel = new DevoirsPanel({
+            showAddButton: !logged.isAdmin(),
+        });
 
-    listItemsLayout.on("render", () => {
-      listItemsLayout.getRegion('panelRegion').show(panel);
-      listItemsLayout.getRegion('itemsRegion').show(devoirsListView);
-    });
+        listItemsLayout.on("render", () => {
+            listItemsLayout.getRegion('panelRegion').show(panel);
+            listItemsLayout.getRegion('itemsRegion').show(devoirsListView);
+        });
 
-    channel.request("region:main").show(listItemsLayout);
-  },
+        devoirsListView.on("item:clone", (childView) => {
+            const model = childView.model;
+            const cloning = model.clone();
+            channel.trigger("loading:up");
+            $.when(cloning).done((newDevoir) => {
+                channel.trigger("data:purge", "exodevoirs");
+                devoirs.add(newDevoir);
+            }).fail((response) => {
+                console.warn("Erreur clonage devoir", response.responseJSON);
+                channel.trigger("popup:error", "Erreur lors du clonage du devoir.");
+            }).always(() => {
+                channel.trigger("loading:down");
+            });
+        });
+
+        channel.request("region:main").show(listItemsLayout);
+    },
 });
 
 export const controller = new Controller();
