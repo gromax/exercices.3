15b73789aecbe5d403040582cf43a40ba3d91191
gromax <gromaxx@gmail.com>
2025-11-16 16:06:38 +0100

    plus de paramètres pour l'initialisation + utilisation des fonctions crées par GraphBloc pour créer les objets graphiques

--- app/components/exercices/tools/blocsviews/graphview.js
+++ app/components/exercices/tools/blocsviews/graphview.js
@@ -4,26 +4,58 @@ const JXG = require('jsxgraph');
 
 const GraphView = View.extend({
   template: graph_tpl,
+  items:null, // tableau de fonction de création recevant graph comme paramètre
+              // et les objets déjà créés
   onRender() {
-    const container = this.el.querySelector('.js-content');
+    const container = this.el.querySelector('.js-jsx');
     const xmin = this.getOption("xmin") || -5;
     const xmax = this.getOption("xmax") || 5;
     const ymin = this.getOption("ymin") || -5;
     const ymax = this.getOption("ymax") || 5;
-    const graph = JXG.JSXGraph.initBoard(container, {
+    const axis = typeof this.getOption("axis") !== "undefined"
+      ? Boolean(this.getOption("axis"))
+      : true;
+    const zoom = typeof this.getOption("zoom") !== "undefined"
+      ? Boolean(this.getOption("zoom"))
+      : false;
+    const pan = typeof this.getOption("pan") !== "undefined"
+      ? Boolean(this.getOption("pan"))
+      : false;
+    const options = {
       boundingbox: [xmin, ymax, xmax, ymin],
-      axis: true
-    });
-
-    for (const element of this.getOption("elements") || []) {
-      if (element.type == "function") {
-        graph.create("functiongraph", [
-          element.function,
-          element.xmin || xmin,
-          element.xmax || xmax
-        ], element.options || {});
-      }
+      axis: axis,
+      showCopyright: false
+    };
+    if (!pan) {
+      options.pan = {
+        enabled:false,
+      };
+      options.showNavigation = false;
     }
+    if (!zoom) {
+      options.zoom = {
+        enabled:false,
+        wheel: false,
+      };
+    }
+    const graph = JXG.JSXGraph.initBoard(container, options);
+    const graphObjects = {};
+    for (const key in this.getOption("items") || {}) {
+      const item = this.getOption("items")[key];
+      if (typeof item !== 'function' || item.length !== 2) {
+        console.warn("GraphView: item non valide", item);
+        continue;
+      }
+      graphObjects[key] = item(graph, graphObjects);
+    }
+    this.graphObjects = graphObjects;
+  },
+
+  children(name) {
+    if (!this.graphObjects) {
+      return undefined;
+    }
+    return this.graphObjects[name];
   }
 });
 
