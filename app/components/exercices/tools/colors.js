46ad2c4ef2a28c9c0fa62b7f4a88d054c0c8999a
gromax <gromaxx@gmail.com>
2025-11-18 22:54:33 +0100

    liste de couleurs et de pictogrammes

---
+++ app/components/exercices/tools/colors.js
@@ -0,0 +1,23 @@
+const COLORS = [
+    'black',
+    'blue',
+    'red',
+    'green',
+    'orange',
+    'purple',
+    'brown',
+    'olive',
+];
+
+const PICTOS = [
+    'square',
+    'circle',
+    'hexagon',
+    'diamond',
+    'star',
+    'pentagon',
+    'heart',
+    'cloud',
+]
+
+export { COLORS, PICTOS };
