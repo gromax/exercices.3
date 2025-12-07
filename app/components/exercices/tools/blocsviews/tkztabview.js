4ca997e41ccee2ce539c9996c104781cc53e96bc
gromax <gromaxx@gmail.com>
2025-12-07 13:30:07 +0100

    ajout de lignes déplacé vers TkzTab + suppression de ; + meilleure gestion du click sur boutons

--- app/components/exercices/tools/blocsviews/tkztabview.js
+++ app/components/exercices/tools/blocsviews/tkztabview.js
@@ -9,50 +9,50 @@ const TkzTabView = View.extend({
     events: {
         'click .js-varline-button': 'onClickTabvar'
     },
+
+    regions: {
+        complement: '.js-complement' // notamment pour stocker les <input>
+    },
     
     onClickTabvar(e) {
-        const target = e.currentTarget;
-        const lineIndex = parseInt(target.getAttribute('data-lineindex'));
-        const xIndex = parseInt(target.getAttribute('data-xindex'));
-        const xpos = target.getAttribute('data-xpos');
-        const ypos = target.getAttribute('data-ypos');
-        const lines = this.getOption('lines');
-        if (!Array.isArray(lines) || lines.length <= lineIndex || lines[lineIndex].type !== 'varform') {
-            console.warn(`Invalid line index or type for var line button click: lineIndex=${lineIndex}`);
-            return;
+        const target = e.currentTarget
+        const lineIndex = parseInt(target.getAttribute('data-lineindex'))
+        const xIndex = parseInt(target.getAttribute('data-xindex'))
+        const xpos = target.getAttribute('data-xpos')
+        const ypos = target.getAttribute('data-ypos')
+        const lines = this.getOption('lines')
+        if (!Array.isArray(lines) || lines.length <= lineIndex || lines[lineIndex].type !== 'inputvar') {
+            console.warn(`Invalid line index or type for var line button click: lineIndex=${lineIndex}`)
+            return
         }
         const line = lines[lineIndex];
         // il s'agit de remplacer le tag correspondant à l'item cliqué
-        const itemTags = line.line.split(',').map( x => x.trim() );
-        const itemTag = itemTags[xIndex].split('/')[0];
-        const newYpos = (ypos === '+') ? '-' : '+';
+        const itemTags = line.line.split(',').map( x => x.trim() )
+        const itemTagComponents = itemTags[xIndex].split('/')
+        const itemTag = itemTagComponents[0]
+        const newYpos = (ypos === '+') ? '-' : '+'
         const newTag = xpos === ''
             ? newYpos
             : xpos === '+'
                 ? itemTag.charAt(0) + itemTag.charAt(1) + newYpos
-                : newYpos + itemTag.charAt(1) + itemTag.charAt(2);
-        itemTags[xIndex] = newTag
+                : newYpos + itemTag.charAt(1) + itemTag.charAt(2)
+        
+        itemTagComponents[0] = newTag
+        const newFullTag = itemTagComponents.join('/')
+        itemTags[xIndex] = newFullTag
         line.line = itemTags.join(',')
-        this.render();
+        this.render()
     },
 
     onRender() {
         this.svgGroup = this.el.querySelector('.tkztab-content');
         const config = this.getOption('config') || {};
         const tkzTab = new TkzTab(this.getOption('xlist'), config);
-        for (let line of this.getOption('lines')) {
-            if (line.type === 'sign') {
-                tkzTab.addSignLine(line.line, line.tag, line.hauteur);
-            } else if (line.type === 'var') {
-                tkzTab.addVarLine(line.line, line.tag, line.hauteur);
-            } else if (line.type === 'varform') {
-                tkzTab.addVarLineForm(line.line, line.tag, line.hauteur);
-            }
-        }
+        tkzTab.addLines(this.getOption('lines'));
         const svgConainer = this.el.querySelector('.tkztab-svg');
         svgConainer.setAttribute('viewBox', `0 0 ${tkzTab.width} ${tkzTab.height}`);
         const draw = SVG().addTo(this.svgGroup);
-        tkzTab.render(draw);
+        tkzTab.render(draw, this.el.querySelector('.js-complement'));
         // On rend les textes en LaTeX
         renderTexInDomElement(this.el);
     }
