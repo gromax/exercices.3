ad3c3ffa3efd7f32febd62f34384038f8ba323b2
gromax <gromaxx@gmail.com>
2025-12-08 00:09:59 +0100

    simplification de la collection qui n'a plus à se comporter en formulaire. La vue item prend en charge le click

--- app/components/exercices/tools/blocsviews/choice.js
+++ app/components/exercices/tools/blocsviews/choice.js
@@ -1,6 +1,7 @@
-import { View, CollectionView } from 'backbone.marionette';
+import { View, CollectionView } from 'backbone.marionette'
 import choice_tpl from '@templates/exercices/bloc/choice-item.jst'
 import form_choice_layout_tpl from '@templates/exercices/bloc/form-choice-layout.jst'
+import renderTexInDomElement from '../../../common/rendertex'
 
 const ChoiceView = View.extend({
     template: choice_tpl,
@@ -10,9 +11,40 @@ const ChoiceView = View.extend({
     templateContext() {
         return {
             button: this.getOption("button") || false
-        };
+        }
+    },
+
+    /**
+     * Répond à un clic sur un item de la liste de choix
+     * @param {number} vmax valeur max pour le choix
+     * @param {Color} colorSet jeu de couleurs
+     * @param {boolean} squareOnly indique si on utilise des pictogrammes ou non
+     * @param {HTMLElement} inputNode nœud input associé
+     * @param {Collection} notshuffledCollection 
+     */
+    itemClick(vmax, colorSet, squareOnly, inputNode, notshuffledCollection) {
+        const model = this.model
+        let idx = model.get('index')
+        idx += 1
+        if (idx > vmax) {
+            idx = 1
+        }
+        model.set({
+            index: idx,
+            color: colorSet.getColor(idx),
+        });
+        if (!squareOnly) {
+            model.set({
+                picto: colorSet.getPicto(idx),
+            })
+        }
+        inputNode.val(
+            notshuffledCollection.map(m => m.get('index')).join('')
+        );
+        this.render();
+        renderTexInDomElement(this.el)
     }
-});
+})
 
 const ChoicesView = CollectionView.extend({
     childView: ChoiceView,
@@ -24,54 +56,24 @@ const ChoicesView = CollectionView.extend({
     },
     className: 'list-group mb-3',
     tagName() {
-        return this.getOption("button") ? 'div' : 'ul';
+        return this.getOption("button") ? 'div' : 'ul'
     }
+    
 });
 
 const ChoiceFormLayout = View.extend({
     template: form_choice_layout_tpl,
     regions: {
-        content: '.js-content',
+        content: '.js-items',
         errors: '.js-validation-error'
-    },
-    triggers: {
-        'submit form': 'submit'
     },
 
     templateContext() {
         return {
             name: this.getOption('name') || 'choice_answer',
             value: this.getOption('value') || '',
-        };
-    },
-
-    onSubmit() {
-        const form = this.el.querySelector('form');
-        const fdata = new FormData(form);
-        const data = Object.fromEntries(fdata.entries());
-        this.dataSubmit(data);
-    },
-
-    dataSubmit(data) {
-        const blocParent = this.getOption("blocParent");
-        if (!blocParent || typeof blocParent.validation !== 'function') {
-            console.warn("Pas de validation possible, bloc parent manquant ou invalide");
-            this.trigger("validation:success", data);
-        return;
         }
-        const error = blocParent.validation();
-        if (error !== null) {
-            this.showError(error);
-            return;
-        }
-        this.trigger("validation:success", data);
-    },
+    }
+})
 
-    showError(error) {
-        const $container = this.$el.find(".js-validation-error");
-        $container.html(`<div class='invalid-feedback d-block'>${error}</div>`);
-    },
-
-});
-
-export { ChoicesView, ChoiceFormLayout };
+export { ChoicesView, ChoiceView, ChoiceFormLayout }
