8fcfca0d4862aad6154cbae9ac4b1864a1645462
gromax <gromaxx@gmail.com>
2025-12-10 18:31:02 +0100

    ajout gestion click sur tabsign + message aide auto + couleur back grise pour cas result

--- app/components/exercices/tools/blocsviews/tkztabview.js
+++ app/components/exercices/tools/blocsviews/tkztabview.js
@@ -2,15 +2,24 @@ import { View } from 'backbone.marionette';
 import { SVG } from '@svgdotjs/svg.js';
 import tkztabview_tpl from '@templates/exercices/bloc/tkztab.jst'
 import renderTexInDomElement from '@common/rendertex';
+import { template } from 'underscore';
 
 const TkzTabView = View.extend({
     template: tkztabview_tpl,
     events: {
-        'click .js-varline-button': 'onClickTabvar'
+        'click .js-varline-button': 'onClickTabvar',
+        'click .js-signline-button': 'onClickTabsign',
+
     },
 
     regions: {
         complement: '.js-complement' // notamment pour stocker les <input>
+    },
+
+    templateContext() {
+        return {
+            showButtonHelp: this.getOption('tkzTab').inputsNumber > 0
+        }
     },
     
     onClickTabvar(e) {
@@ -19,6 +28,14 @@ const TkzTabView = View.extend({
         const xIndex = parseInt(target.getAttribute('data-xindex'))
         const xpos = target.getAttribute('data-xpos')
         this.getOption('tkzTab').togglePosItem(lineIndex, xIndex, xpos)
+        this.render()
+    },
+
+    onClickTabsign(e) {
+        const target = e.currentTarget
+        const lineIndex = parseInt(target.getAttribute('data-lineindex'))
+        const xIndex = parseInt(target.getAttribute('data-xindex'))
+        this.getOption('tkzTab').toggleSign(lineIndex, xIndex)
         this.render()
     },
 
@@ -31,7 +48,13 @@ const TkzTabView = View.extend({
         tkzTab.render(draw, this.el.querySelector('.js-complement'));
         // On rend les textes en LaTeX
         renderTexInDomElement(this.el);
-    }
+    },
+
+    className() {
+        return this.getOption("result") === true
+            ? 'list-group-item list-group-item-dark'
+            : ''
+    },
 });
 
 export default TkzTabView;
