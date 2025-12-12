c27d5fd94f822f7a2c583f4e796a4ddde3984c39
gromax <gromaxx@gmail.com>
2025-12-12 20:10:40 +0100

    gros changements. Séparation du cas avec enfants

--- app/components/exercices/tools/maths/mymath.js
+++ app/components/exercices/tools/maths/mymath.js
@@ -13,19 +13,95 @@ import Parser from './parser/parser';
 import { build } from './parser/rpnbuilder';
 import { substituteLabels, getValue } from './misc/substitution';
 
-class MyNath {
-    /** @type{Array|null} enfants de l'objet */
-    #children
+// Constante privée
+// sert à empêcher l'accès direct au constructeur
+const PRIVATE = Symbol('private');
 
+class MyMathArray {
+    /** @type{Array<MyMath>|null} enfants de l'objet */
+    #children = null
+
+    /**
+     * constructeur
+     * @param {Array<MyMath>} children 
+     */
+    constructor(children) {
+        this.#children = children
+    }
+
+    get expression() {
+        return this.#children.map(child => child.expression)
+    }
+
+    get variables() {
+        return this.#children.map(child => child.variables)
+    }
+
+    toFloat() {
+        return this.#children.map(child => child.toFloat());
+    }
+
+    toString() {
+        return this.#children.map(child => child.toString());
+    }
+
+    latex() {
+        return this.#children.map(child => child.latex());
+    }
+
+    toFormat(format) {
+        return this.#children.map(child => child.toFormat(format));
+    }
+
+    expand() {
+        return new MyMathArray(this.#children.map(child => child.expand()))
+    }
+
+    sub(varName, value) {
+        return new MyMath(PRIVATE, this.#children.map(child => child.sub(varName, value)))
+    }
+
+    buildFunction() {
+        return this.#children.map(child => child.buildFunction())
+    }
+
+    compare(rightExpr, operator, params = {}) {
+        if (Array.isArray(rightExpr) || (rightExpr instanceof MyMathArray)) {
+            if (rightExpr.length !== this.#children.length) {
+                throw new Error(`Les tableaux comparés n'ont pas la même taille.`);
+            }
+            if (Array.isArray(rightExpr)) {
+                return this.#children.map((child, index) => child.compare(rightExpr[index], operator, params))
+            } else {
+                return this.#children.map((child, index) => child.compare(rightExpr.getChild(index), operator))
+            }
+        }
+        return this.#children.map(child => child.compare(rightExpr, operator, params));
+    }
+
+    get length() {
+        return this.#children.length;
+    }
+
+    getChild(index) {
+        return this.#children[index]
+    }
+
+}
+
+
+class MyMath {
     /** @type{string} expression d'origine */
     #expression = ""
 
     /** @type{nerdamer.Expression} */
-    #processed
+    #nerdamer_processed
 
     /** @type{string} */
     #processed_lower_text = ""
 
+    /** @type{string} */
+    #normalized
 
     static reverseOperator(operator) {
         switch (operator) {
@@ -50,7 +126,7 @@ class MyNath {
         if (typeof value === 'number') {
             return value;
         }
-        if (value instanceof MyNerd) {
+        if (value instanceof MyMath) {
             return value.toFloat();
         }
         if (typeof value !== 'string') {
@@ -60,11 +136,11 @@ class MyNath {
         if (/^[+-]?\s?inf(?:inity|ty|ini)?$/.test(value)) {
             return value.startsWith('-') ? -Infinity : Infinity;
         }
-        return MyNerd.toFloat(value);
+        return MyMath.toFloat(value);
     }
 
     static parseInt(value) {
-        const f = MyNerd.parseFloat(value);
+        const f = MyMath.parseFloat(value);
         const n = Math.trunc(f);
         if (f !== n) {
             throw new Error(`La valeur ${value} ne peut pas être convertie en entier.`);
@@ -72,58 +148,70 @@ class MyNath {
         return n;
     }
 
+    /**
+     * fabrique un MyMath à partir d'une expression
+     * @param {string|Array|MyMath|MyMathArray} expression 
+     * @param {object} params 
+     * @returns {MyMath|MyMathArray}
+     */
     static make(expression, params = {}) {
-        return new MyNerd(expression, params);
+        if ((expression instanceof MyMath) || (expression instanceof MyMathArray)) {
+            return expression
+        }
+        if (Array.isArray(expression)) {
+            return new MyMathArray(expression.map(expr => MyMath.make(expr, params)))
+        }
+        return new MyMath(PRIVATE, expression, params)
     }
 
     static latex(expression, params = {}) {
         if (/^[+-]?\s*(?:∞|inf|infinity|infty|infini)?$/.test(expression)) {
             return expression.startsWith('-') ? "-\\infty" : "+\\infty";
         }
-        const n = new MyNerd(expression, params);
+        const n = new MyMath(PRIVATE, expression, params);
         return n.latex();
     }
     
     static parseUser(expression) {
         // user ne va pas forcément respecter les * ou ce genre de détails
-        // je vais donc preprocesser
+        // je vais donc préprocesser
         if (/^[+-]?\s*(?:∞|inf|infinity|infty|infini)?$/.test(expression)) {
             expression = expression.startsWith('-') ? "-infinity" : "infinity";
         }
         try {
             const parsed = new Parser(expression);
             const b = build(parsed.rpn);
-            return new MyNerd(b.toString());
+            return new MyMath(PRIVATE, b.toString());
         } catch (e) {
             console.warn("Erreur lors du parsing de l'expression utilisateur :", expression);
-            return new MyNerd("NaN");
+            return new MyMath(PRIVATE, "NaN");
         }
     }
 
     static toFloat(expression, params = {}) {
-        const n = new MyNerd(expression, params);
+        const n = new MyMath(PRIVATE, expression, params);
         return n.toFloat();
     }
 
     static toFormat(expression, format, params = {}) {
-        const n = new MyNerd(expression, params);
+        const n = new MyMath(PRIVATE, expression, params);
         return n.toFormat(format);
     }
 
     static variables(expression, params = {}) {
-        const n = new MyNerd(expression, params);
+        const n = new MyMath(PRIVATE, expression, params);
         return n.variables;
     }
 
     static buildFunction(expression, params = {}) {
-        const n = new MyNerd(expression, params);
+        const n = new MyMath(PRIVATE, expression, params);
         return n.buildFunction();
     }
 
     static solveInC(exprLeft, exprRight, varName) {
         try {
             const equation = `${exprLeft} = ${exprRight}`;
-            const normalized = MyNerd.normalization(equation);
+            const normalized = MyMath.normalization(equation);
             const solutions = nerdamer.solveEquations(normalized, varName);
             return solutions.toString().split(',');
         } catch (e) {
@@ -133,7 +221,7 @@ class MyNath {
     }
 
     static solveInR(exprLeft, exprRight, varName) {
-        const solutionsStr = MyNerd.solveInC(exprLeft, exprRight, varName);
+        const solutionsStr = MyMath.solveInC(exprLeft, exprRight, varName);
         // je filtre les solutions complexes
         return solutionsStr.filter(sol => !sol.includes('i'));
     }
@@ -147,7 +235,7 @@ class MyNath {
      * @returns {boolean} le résultat de la comparaison
      */
     static compare(leftExpr, rightExpr, operator, params = {}) {
-        const left = MyNerd.make(leftExpr, params);
+        const left = MyMath.make(leftExpr, params);
         return left.compare(rightExpr, operator, params);
     }
 
@@ -199,61 +287,53 @@ class MyNath {
      */
     static substituteExpressions(texte, params) {
         return texte.replace(/\{([^:{}]+):\s*([\w]*(?:\$)?)?\}/g, (match, expr, format) => {
-            return MyNerd.make(expr, params).toFormat(format);
+            return MyMath.make(expr, params).toFormat(format);
         });
     }
 
 
     // Méthodes d'instance
-
-    constructor(expression, params = {}) {
+    constructor(token, expression, params = {}) {
+       if (token !== PRIVATE) {
+            throw new Error('Utilisez MyMath.make() pour créer une instance')
+        } 
         if (typeof expression !== 'string') {
-            expression = String(expression);
+            expression = String(expression)
         }
         this.#expression = expression.includes('@')
           ? getValue(expression, params) ?? substituteLabels(expression, params)
-          : expression;
-        if (this.#expression === Infinity || this.#expression === "Infinity") {
-            this.#expression = "infinity";
-        } else if (this.#expression === -Infinity || this.#expression === "-Infinity") {
-            this.#expression = "-infinity";
+          : expression
+        if (this.#expression === "Infinity") {
+            this.#expression = "infinity"
+        } else if (this.#expression === "-Infinity") {
+            this.#expression = "-infinity"
         }
-          this.#children = null;
-        if (Array.isArray(this.#expression)) {
-            this.#children = this.#expression.map(expr => new MyNerd(expr, params));
-            return;
-        }
-        this._normalized = MyNerd.normalization(this.#expression);
+        this.#normalized = MyMath.normalization(this.#expression)
         try {
-            this.#processed = nerdamer(this._normalized);
-            this.#processed_lower_text = this.#processed.text().toLowerCase();
+            this.#nerdamer_processed = nerdamer(this.#normalized)
+            this.#processed_lower_text = this.#nerdamer_processed.text().toLowerCase()
         } catch (e) {
-            console.warn(`Erreur lors du traitement avec nerdamer de ${this._normalized}:`, e);
-            this.#processed = nerdamer("NaN");
-            this.#processed_lower_text = this.#processed.text().toLowerCase();
+            console.warn(`Erreur lors du traitement avec nerdamer de ${this.#normalized}:`, e)
+            this.#nerdamer_processed = nerdamer("NaN")
+            this.#processed_lower_text = this.#nerdamer_processed.text().toLowerCase()
         }
     }
 
+    #getNerdamerProcessed() {
+        return this.#nerdamer_processed;
+    }
+
     get expression() {
-        if (this.#children !== null) {
-            return this.#children.map(child => child.expression);
-        }
         return this.#expression;
     }
 
     get variables() {
-        if (this.#children !== null) {
-            return this.#children.map(child => child.variables);
-        }
-        return this.#processed.variables();
+        return this.#nerdamer_processed.variables();
     }
 
     toFloat() {
-        if (this.#children !== null) {
-            return this.#children.map(child => child.toFloat());
-        }
         try {
-            const txt = this.#processed.evaluate().text('decimals');
+            const txt = this.#nerdamer_processed.evaluate().text('decimals');
             if (txt === 'infinity') {
                 return Infinity;
             } else if (txt === '-infinity') {
@@ -267,23 +347,17 @@ class MyNath {
     }
 
     toString() {
-        if (this.#children !== null) {
-            return this.#children.map(child => child.toString());
-        }
-        return MyNerd.denormalization(this.#processed.toString());
+        return MyMath.denormalization(this.#nerdamer_processed.toString());
     }
 
     latex() {
-        if (this.#children !== null) {
-            return this.#children.map(child => child.latex());
-        }
-        const txt = this.#processed.text();
+        const txt = this.#nerdamer_processed.text();
         if (txt === 'infinity') {
             return "+\\infty";
         } else if (txt === '-infinity') {
             return "-\\infty";
         }
-        return MyNerd.latexDenormalization(this._processed.toTeX());
+        return MyMath.latexDenormalization(this._processed.toTeX());
     }
 
     /**
@@ -295,9 +369,6 @@ class MyNath {
      * @returns {string} la valeur formatée
      */
     toFormat(format) {
-        if (this.#children !== null) {
-            return this.#children.map(child => child.toFormat(format));
-        }
         format = (format || '').trim();
         if (format === '$') {
             return this.latex();
@@ -329,11 +400,11 @@ class MyNath {
      */
     #toFormatDecimal(n) {
         if (n >= 0) {
-            return MyNerd.denormalization(this._processed.evaluate().text('decimals', n));
+            return MyMath.denormalization(this._processed.evaluate().text('decimals', n));
         }
         // Si on a une expression comme "4.3 + sqrt(4.5)", nerdamer ne va pas
         // exécuter la fonction. Dans ce cas il me semble plus pertinent d'évaluer
-        return MyNerd.denormalization(this._processed.evaluate().text('decimals'));
+        return MyMath.denormalization(this._processed.evaluate().text('decimals'));
     }
 
     /**
@@ -362,37 +433,30 @@ class MyNath {
         return b.simplify().toTex();
     }
 
-
     compare(rightExpr, operator, params = {}) {
-        if (this.#children !== null) {
-            if (Array.isArray(rightExpr)) {
-                if (rightExpr.length !== this.#children.length) {
-                    throw new Error(`Les tableaux comparés n'ont pas la même taille.`);
-                }
-                return this.#children.map((child, index) => child.compare(rightExpr[index], operator, params));
-            } else {
-                return this.#children.map(child => child.compare(rightExpr, operator, params));
-            }
+        const right = MyMath.make(rightExpr, params);
+        if (right instanceof MyMathArray) {
+            // c'est un objet MyMathArray
+            return right.compare(this, MyMath.reverseOperator(operator));
         }
-        const right = MyNerd.make(rightExpr, params);
         if (this.isInfinity()) {
             return this.#compareInfinityCase(right, operator);
         } else if (right.isInfinity()) {
-            return right.#compareInfinityCase(this, MyNerd.reverseOperator(operator));
+            return right.#compareInfinityCase(this, MyMath.reverseOperator(operator));
         }
         switch (operator) {
             case '==':
-                return this.#processed.eq(right.#processed);
+                return this.#nerdamer_processed.eq(right.#nerdamer_processed);
             case '!=':
-                return !this.#processed.eq(right.#processed);
+                return !this.#nerdamer_processed.eq(right.#nerdamer_processed);
             case '<':
-                return this.#processed.lt(right.#processed);
+                return this.#nerdamer_processed.lt(right.#nerdamer_processed);
             case '<=':
-                return this.#processed.lte(right.#processed);
+                return this.#nerdamer_processed.lte(right.#nerdamer_processed);
             case '>':
-                return this.#processed.gt(right.#processed);
+                return this.#nerdamer_processed.gt(right.#nerdamer_processed);
             case '>=':
-                return this.#processed.gte(right.#processed);
+                return this.#nerdamer_processed.gte(right.#nerdamer_processed);
             default:
                 throw new Error(`Opérateur de comparaison invalide : ${operator}`);
         }
@@ -418,43 +482,30 @@ class MyNath {
     }
 
     isInfinity() {
-        return this.#children === null && (this.#processed_lower_text === 'infinity' || this.#processed_lower_text === '-infinity');
+        return this.#processed_lower_text === 'infinity' || this.#processed_lower_text === '-infinity';
     }
 
     isPlusInfinity() {
-        return this.#children === null && this.#processed_lower_text === 'infinity';
+        return this.#processed_lower_text === 'infinity';
     }
 
     isMinusInfinity() {
-        return this.#children === null && this.#processed_lower_text === '-infinity';
+        return this.#processed_lower_text === '-infinity';
     }
 
     expand() {
-        if (this.#children !== null) {
-            for (const child of this.#children) {
-                child.expand();
-            }
-            return this;
-        }
-        this.#processed = this.#processed.expand();
-        return this;
+        return new MyMath(PRIVATE, MyMath.denormalization(this.#nerdamer_processed.expand().toString()));
     }
 
     sub(varName, value) {
-        if (this.#children !== null) {
-            return new MyNerd(this.#children.map(child => child.sub(varName, value)));
-        }
-        this.#processed = this.#processed.sub(varName, value);
+        this.#nerdamer_processed = this.#nerdamer_processed.sub(varName, value);
         return this;
     }
 
     buildFunction() {
-        if (this.#children !== null) {
-            return this.#children.map(child => child.buildFunction());
-        }
-        return this.#processed.buildFunction();
+        return this.#nerdamer_processed.buildFunction();
     }
 
 }
 
-export default MyNath;
+export default MyMath;
