989468fa88d6ff378816d4fae7ea980b9cb8e901
gromax <gromaxx@gmail.com>
2025-12-16 18:46:10 +0100

    cosmétique

--- app/components/header/view.js
+++ app/components/header/view.js
@@ -3,53 +3,48 @@ import header_tpl from '@templates/header/show/header-navbar.jst';
 import Radio from 'backbone.radio';
 
 const HeaderView = View.extend({
-  template: header_tpl,
-  logged: { isOff: true },
-  triggers: {
-    "click a.js-home": "home:show",
-    "click a.js-edit-me": "home:editme",
-    "click a.js-login": "home:login",
-    "click a.js-logout": "logout",
-    "click a.js-message": "messages:list"
-  },
+    template: header_tpl,
+    logged: { isOff: true },
+    triggers: {
+        "click a.js-home": "home:show",
+        "click a.js-edit-me": "home:editme",
+        "click a.js-login": "home:login",
+        "click a.js-logout": "logout",
+        "click a.js-message": "messages:list"
+    },
 
-  events: {
-    "click button.js-hamburger": "onClickHamburger"
-  },
+    events: {
+        "click button.js-hamburger": "onClickHamburger"
+    },
 
-  onClickHamburger(e){
-    // le bouton bootstrap ne fait pas le collapse tout seul car le header
-    // j'ai donc ôté la cible data-bs-target et je gère le toggle moi-même
-    e.preventDefault();
-    const div = $("#navbarContent")
-    div.toggleClass("show")
+    onClickHamburger(e){
+        // le bouton bootstrap ne fait pas le collapse tout seul car le header
+        // j'ai donc ôté la cible data-bs-target et je gère le toggle moi-même
+        e.preventDefault();
+        const div = $("#navbarContent")
+        div.toggleClass("show")
+    },
 
-    console.log("click hamburger")
-  },
+    showSpinner(){
+        $("span.js-spinner", this.$el).html("<i class='fa fa-spinner fa-spin'></i>");
+    },
 
-  showSpinner(){
-    $("span.js-spinner", this.$el).html("<i class='fa fa-spinner fa-spin'></i>");
-  },
+    hideSpinner(){
+        $("span.js-spinner", this.$el).html("");
+    },
 
-  hideSpinner(){
-    $("span.js-spinner", this.$el).html("");
-  },
-
-  templateContext() {
-    const logged = Radio.channel("app").request("logged:get");
-    return {
-      isRoot: logged.isRoot(),
-      isAdmin: logged.isAdmin(),
-      isProf: logged.isProf(),
-      isEleve: logged.isEleve(),
-      isOff: logged.isOff(),
-      nomComplet: logged.get("nomComplet"),
-      unread: (logged.unread || 0),
-    };
-  },
-
-
-
+    templateContext() {
+        const logged = Radio.channel("app").request("logged:get");
+        return {
+            isRoot: logged.isRoot(),
+            isAdmin: logged.isAdmin(),
+            isProf: logged.isProf(),
+            isEleve: logged.isEleve(),
+            isOff: logged.isOff(),
+            nomComplet: logged.get("nomComplet"),
+            unread: (logged.unread || 0),
+        };
+    },
 });
 
 export { HeaderView };
