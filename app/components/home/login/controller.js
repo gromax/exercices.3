83e96202b116e69938a6055d5d6a25c01208964f
gromax <gromaxx@gmail.com>
2025-12-16 18:46:57 +0100

    controle vue pour forgotten

--- app/components/home/login/controller.js
+++ app/components/home/login/controller.js
@@ -1,74 +1,75 @@
-import { MnObject, Region } from 'backbone.marionette';
-import { LoginView } from './view.js';
+import { MnObject } from 'backbone.marionette'
+import { LoginView, ForgottenView } from './view'
 
 const Controller = MnObject.extend({
-  channelName: "app",
-  showLogin() {
-    const channel = this.getChannel();
-    const view = new LoginView({
-      model: channel.request("logged:get"),
-      showForgotten:true
-    });
-    view.on("success", (data) => {
-      channel.trigger("home:show");
-    });
+    channelName: "app",
+    showLogin() {
+        const channel = this.getChannel();
+        const view = new LoginView({
+            model: channel.request("logged:get"),
+            showForgotten:true
+        });
+        view.on("success", (data) => {
+            channel.trigger("home:show");
+        });
 
-    view.on("login:forgotten", (email) => {
-      // Vérification de l'email
-      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      if (!re.test(email)){
-        view.triggerMethod("form:data:invalid", [{ success:false, message: "L'email n'est pas valide"}]);
-      } else {
-        channel.trigger("loading:up");
-        let sendingMail = channel.request("forgotten:password", email);
-        sendingMail.done( function(response){
-          channel.trigger("show:message:success", {
-            title:"Email envoyé",
-            message:"Un message a été envoyé à l'adresse #{email}. Veuillez vérifier dans votre boîte mail et cliquer sur le lien contenu dans le mail. [Cela peut prendre plusieurs minutes...]"
-          });
-        }).fail( function(response){
-          channel.trigger("data:fetch:fail", response, "033");
-        }).always( function(){
-          channel.trigger("loading:down");
+        channel.request("region:main").show(view);
+    },
+
+    showForgottenAsk() {
+        const channel = this.getChannel()
+        const view = new ForgottenView()
+        view.on("forgotten:ask", (email) => {
+            channel.trigger("loading:up")
+            const sending = channel.request("session:forgotten:ask", email)
+            $.when(sending).done( function(response){
+                channel.trigger("popup:success", {
+                  title:"Email envoyé",
+                  message:`Si cet email existe, un message a été envoyé à l'adresse ${email}. Veuillez vérifier dans votre boîte mail et cliquer sur le lien contenu dans le mail. [Cela peut prendre plusieurs minutes...]`
+                });
+            }).fail( function(response){
+                channel.trigger("data:fetch:fail", response)
+            }).always( function(){
+                channel.trigger("loading:down");
+            })
         })
-      }
-    });
-    channel.request("region:main").show(view);
-  },
-  showReLogin(options) {
-    let that = this;
-    let view = new LoginView({ generateTitle: false, showForgotten:false, title:"Reconnexion" });
-    this.listenTo(view, "dialog:closed", function(){
-      return (options && typeof options.fail == 'function') && options.fail();
-    });
+        channel.request("region:main").show(view);
+    },
 
-    const channel = this.getChannel();
+    showReLogin(options) {
+        let that = this;
+        let view = new LoginView({ generateTitle: false, showForgotten:false, title:"Reconnexion" });
+        this.listenTo(view, "dialog:closed", function(){
+          return (options && typeof options.fail == 'function') && options.fail();
+        });
 
-    view.on("form:submit", function(data){
-      const logged = sessionChannel.request("get");
-      if ((data.identifiant === "") || (data.identifiant === logged.get("identifiant"))){
-        // C'est bien la même personne qui se reconnecte
-        let openingSession = logged.save(data);
-        if (openingSession){
-          channel.trigger("loading:up");
-          $.when(openingSession).done( function(response){
-            that.stopListening();
-            view.trigger("dialog:close");
-            return (options && typeof options.done == 'function') && options.done();
-          }).fail( function(response){
-            channel.trigger("data:fetch:fail", response, "025");
-          }).always( function(){
-            channel.trigger("loading:down");
-          });
-        } else {
-          view.triggerMethod("form:data:invalid", logged.validationError);
-        }
-      } else {
-        view.triggerMethod("form:data:invalid", [{success:false, message:"C'est une reconnexion : Vous devez réutiliser le même identifiant que précedemment."}]);
-      }
-    });
-    channel.request("region:main").show(view);
-  }
+        const channel = this.getChannel();
+
+        view.on("form:submit", function(data){
+            const logged = sessionChannel.request("get");
+            if ((data.identifiant === "") || (data.identifiant === logged.get("identifiant"))){
+                // C'est bien la même personne qui se reconnecte
+                let openingSession = logged.save(data);
+                if (openingSession){
+                    channel.trigger("loading:up");
+                    $.when(openingSession).done( function(response){
+                        that.stopListening();
+                        view.trigger("dialog:close");
+                        return (options && typeof options.done == 'function') && options.done();
+                    }).fail( function(response){
+                        channel.trigger("data:fetch:fail", response, "025");
+                    }).always( function(){
+                        channel.trigger("loading:down");
+                    });
+                } else {
+                    view.triggerMethod("form:data:invalid", logged.validationError);
+                }
+            } else {
+                view.triggerMethod("form:data:invalid", [{success:false, message:"C'est une reconnexion : Vous devez réutiliser le même identifiant que précedemment."}]);
+            }
+        });
+        channel.request("region:main").show(view);
+    }
 });
 
 const controller = new Controller();
