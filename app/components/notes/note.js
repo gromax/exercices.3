c23feb506fb0b5f366f19a7fe671fb271ea04ba3
gromax <gromaxx@gmail.com>
2025-12-22 21:17:03 +0100

    cosmetic

--- app/components/notes/note.js
+++ app/components/notes/note.js
@@ -2,54 +2,54 @@ import { MyModel, MyCollection } from "../common/entity.js";
 import Misc from "../common/misc.js";
 
 const Item = MyModel.extend ({
-  urlRoot: "api/notes",
-  readOnly: true,
+    urlRoot: "api/notes",
+    readOnly: true,
 
-  defaults: {
-    nom:"", // nom du devoir
-    idOWner: null, // id du créateur du devoir
-    idClasse: null, // id de la classe du devoir
-    idDevoir: null, // id du devoir
-    idUser:null, // id de l'élève
-    nomClasse:"", // nom de la classe du devoir
-    nomOwner:"", // nom du créateur du devoir
-    nomUser:"", // nom de l'élève
-    prenomUser:"", // prénom de l'élève
-    dateDebut:"", // date de début du devoir
-    dateFin:"", // date de fin du devoir
-    note:0, // note de l'élève
-    actif: true, // si le devoir est actif ou non
-    timeLeft: null, // temps restant pour rendre le devoir
-    exoCount:0 // nombre d'exercices liés
-  },
+    defaults: {
+        nom:"", // nom du devoir
+        idOWner: null, // id du créateur du devoir
+        idClasse: null, // id de la classe du devoir
+        idDevoir: null, // id du devoir
+        idUser:null, // id de l'élève
+        nomClasse:"", // nom de la classe du devoir
+        nomOwner:"", // nom du créateur du devoir
+        nomUser:"", // nom de l'élève
+        prenomUser:"", // prénom de l'élève
+        dateDebut:"", // date de début du devoir
+        dateFin:"", // date de fin du devoir
+        note:0, // note de l'élève
+        actif: true, // si le devoir est actif ou non
+        timeLeft: null, // temps restant pour rendre le devoir
+        exoCount:0 // nombre d'exercices liés
+    },
 
-  toString() {
-    return `Note de ${this.get("prenomUser")} ${this.get("nomUser")} pour le devoir ${this.get("nom")} : ${this.get("note")} pts`;
-  },
+    toString() {
+        return `Note de ${this.get("prenomUser")} ${this.get("nomUser")} pour le devoir ${this.get("nom")} : ${this.get("note")} pts`;
+    },
 
-  parse(data) {
-    data.idOwner = Number(data.idOwner);
-    data.idDevoir = Number(data.idDevoir);
-    data.idUser = Number(data.idUser);
-    data.isClasse = Number(data.isClasse);
-    data.note = Number(data.note);
-    data.actif = Misc.parseBoolean(data.actif);
-    data.exoCount = Number(data.exoCount);
-    const timeToEnd = Misc.computeTimeFromNowToDate(data.dateFin, true);
-    const timeToBegin = Misc.computeTimeFromNowToDate(data.dateDebut, false);
-    data.notStarted = (timeToBegin > 0);
-    data.notEnded = (timeToEnd > 0);
-    data.timeLeft = Misc.computeTimeLeft(data.dateDebut, data.dateFin);
-    data.actif = (data.timeLeft !== null);
-    data.dateDebutFr = Misc.formatDateFrench(data.dateDebut);
-    data.dateFinFr = Misc.formatDateFrench(data.dateFin);
-    return data;
-  },
+    parse(data) {
+        data.idOwner = Number(data.idOwner);
+        data.idDevoir = Number(data.idDevoir);
+        data.idUser = Number(data.idUser);
+        data.isClasse = Number(data.isClasse);
+        data.note = Number(data.note);
+        data.actif = Misc.parseBoolean(data.actif);
+        data.exoCount = Number(data.exoCount);
+        const timeToEnd = Misc.computeTimeFromNowToDate(data.dateFin, true);
+        const timeToBegin = Misc.computeTimeFromNowToDate(data.dateDebut, false);
+        data.notStarted = (timeToBegin > 0);
+        data.notEnded = (timeToEnd > 0);
+        data.timeLeft = Misc.computeTimeLeft(data.dateDebut, data.dateFin);
+        data.actif = (data.timeLeft !== null);
+        data.dateDebutFr = Misc.formatDateFrench(data.dateDebut);
+        data.dateFinFr = Misc.formatDateFrench(data.dateFin);
+        return data;
+    },
 });
 
 const Collection = MyCollection.extend({
-  url: "api/notes",
-  model: Item,
+    url: "api/notes",
+    model: Item,
 });
 
 export { Item, Collection };
