4c3a23cada2e17a7298175ee85286d89cbd59b1d
gromax <gromaxx@gmail.com>
2026-01-29 00:06:58 +0100

    ajout implémentation pour les blocs susceptibles de contenir une réponse

---
+++ app/components/exercices/tools/exocode/implementation/formitem.ts
@@ -0,0 +1,12 @@
+import { View } from "backbone.marionette"
+
+type AnyView = View<any>|Array<View<any>>
+
+interface FormItemImplementation {
+    resultView(userData:Record<string, string>):AnyView
+    resultScore(userData:Record<string, string>):number
+    validation(userValue?:string|Array<string>):string|Array<string>|boolean|Record<string, string>
+    nombrePts():number
+}
+
+export default FormItemImplementation
