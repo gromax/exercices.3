bbc829ff79fd261610d53c3180328bfd45416e03
gromax <gromaxx@gmail.com>
2026-09-05 20:54:13 +0200

    documentation + indent

--- php/class/BDDObject/InitKey.php
+++ php/class/BDDObject/InitKey.php
@@ -7,58 +7,72 @@ use PDOException;
 
 final class InitKey extends Item
 {
-  protected static $BDDName = "initkeys";
-  ##################################### METHODES STATIQUES #####################################
+    protected static $BDDName = "initkeys";
+    ##################################### METHODES STATIQUES #####################################
 
-  /**
-   * Créer une clé de réinitialisation pour un utilisateur donné
-   * @param int $id id de l'utilisateur
-   * @return InitKey l'objet InitKey créé
-   */
-  public static function createKey($idUser)
-  {
-    $token = bin2hex(random_bytes(32));
-    $initKey = new InitKey(array(
-      'initKey' => $token,
-      'idUser' => $idUser
-    ));
-    return $initKey;
-  }
-
-  protected static function champs()
-  {
-    return [
-      'initKey' => ['def' => "", 'type'=> 'string'],    // clé de réinitialisation
-      'idUser' => ['def' => 0, 'type'=> 'integer'],         // id de l'utilisateur
-    ] ;
-  }
-
-  public function delete()
-  {
-    if (!$this->okToDelete()) {
-      EC::addError(static::$BDDName."/delete : L'objet a des enfants impossibles à supprimer.");
-      return false;
+    /**
+     * Créer une clé de réinitialisation pour un utilisateur donné
+     * @param int $idUser id de l'utilisateur
+     * @return InitKey l'objet InitKey créé
+     */
+    public static function createKey($idUser)
+    {
+        $token = bin2hex(random_bytes(32));
+        $initKey = new InitKey(array(
+            'initKey' => $token,
+            'idUser' => $idUser
+        ));
+        return $initKey;
     }
-  }
 
-  static public function deleteFromIdUser($idUser)
-  {
-    require_once BDD_CONFIG;
-    try {
-      $pdo=new PDO(BDD_DSN,BDD_USER,BDD_PASSWORD);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $stmt = $pdo->prepare("DELETE FROM ".PREFIX_BDD.static::$BDDName." WHERE idUser = :id");
-      $stmt->bindValue(':id', $idUser, PDO::PARAM_INT);
-      $stmt->execute();
-      EC::add("Keys supprimées avec succès.");
-      return true;
-    } catch(PDOException $e) {
-      EC::addError($e->getMessage(), "initKey/deleteFromIdUser");
-      return false;
+    /**
+     * Renvoie les champs de l'objet InitKey
+     * @return array Tableau des champs
+     */
+    protected static function champs()
+    {
+        return [
+            'initKey' => ['def' => "", 'type'=> 'string'],        // clé de réinitialisation
+            'idUser' => ['def' => 0, 'type'=> 'integer'],                 // id de l'utilisateur
+        ] ;
     }
-  }
 
-  ##################################### METHODES #####################################
+    /**
+     * Supprime l'objet InitKey actuel si possible
+     * @return bool true si la suppression a réussi, false sinon
+     */
+    public function delete()
+    {
+        if (!$this->okToDelete()) {
+            EC::addError(static::$BDDName."/delete : L'objet a des enfants impossibles à supprimer.");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Supprime toutes les clés de réinitialisation associées à un utilisateur donné
+     * @param int $idUser id de l'utilisateur
+     * @return bool true si la suppression a réussi, false sinon
+     */
+    static public function deleteFromIdUser($idUser)
+    {
+        require_once BDD_CONFIG;
+        try {
+            $pdo=new PDO(BDD_DSN,BDD_USER,BDD_PASSWORD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $pdo->prepare("DELETE FROM ".PREFIX_BDD.static::$BDDName." WHERE idUser = :id");
+            $stmt->bindValue(':id', $idUser, PDO::PARAM_INT);
+            $stmt->execute();
+            EC::add("Keys supprimées avec succès.");
+            return true;
+        } catch(PDOException $e) {
+            EC::addBDDError($e->getMessage(), "initKey/deleteFromIdUser");
+            return false;
+        }
+    }
+
+    ##################################### METHODES #####################################
 
 
 }
