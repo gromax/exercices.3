ffe31e670fe7b109ccb0ec3eb9b9ac9e06bad7f1
gromax <gromaxx@gmail.com>
2026-09-06 18:23:17 +0200

    amélioration de la customisation

--- app/components/exercices/tools/exocode/blocs/graphitems/reels.ts
+++ app/components/exercices/tools/exocode/blocs/graphitems/reels.ts
@@ -1,28 +1,34 @@
 import JXG from 'jsxgraph'
 import GraphItem from "./item"
 import _ from "underscore"
+import { getNumberOption, getOption } from "../../misc"
 
 class GraphReels extends GraphItem {
     _type = 'Function'
     public createJXGItem(g:JXG.Board, graphObjects:Record<string, JXG.GeometryElement>):JXG.GeometryElement {
-        const y = this.item.params.y !== undefined ? Number(this.item.params.y) : 0
-        const color = this.item.params.color || 'black'
-        const titleSize = this.item.params.titlesize || 20
-        const tickSize = this.item.params.ticksize || 12
+        const y = getNumberOption(this.item.params, 'y', 0)
+        const color = getOption(this.item.params, 'color', 'black')
+        const titleSize = getNumberOption(this.item.params, 'titlesize', 20)
+        const tickSize = getNumberOption(this.item.params, 'ticksize', 12)
+        const majorHeight = getNumberOption(this.item.params, 'majorheight', 10)
+        const minorHeight = getNumberOption(this.item.params, 'minorheight', 5)
+        const minorTicks = getNumberOption(this.item.params, 'minorticks', 4)
+        const strokeWidth = getNumberOption(this.item.params, 'strokewidth', 1)
+        
         const options = {
             withLabel: true,
             lastArrow:true,
             strokeColor: color,
+            strokeWidth: strokeWidth,
             ticks: {
                 drawLabels: true,
-                majorHeight: 10,
-                minorHeight: 5,
-                minorTicks: 4,
-                includeBoundaries: true,  // ← force l'affichage du 0
+                majorHeight: majorHeight,
+                minorHeight: minorHeight,
+                minorTicks: minorTicks,
+                drawZero: true,  // ← force l'affichage du 0
                 strokeColor: color,
                 label: {
-                    position: "rt",
-                    offset: [0,-10],
+                    offset: [0,-tickSize],
                     fontSize: tickSize,
                     color: color
                 }
