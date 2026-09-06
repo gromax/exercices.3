cf5fa8da16a0cf9d1c842c78ea8826f0abed5eef
gromax <gromaxx@gmail.com>
2026-09-06 18:20:09 +0200

    Ajout type NestedString

--- app/components/types/InputType.ts
+++ app/components/types/InputType.ts
@@ -1,6 +1,5 @@
 import MyMath from "@mathstools/mymath";
 type NestedArray<T> = T | Array<NestedArray<T>>
-
 export type NestedInput = NestedArray<InputType>
 export type InputType = MyMath|number|string
 export type TParams = Record<string, NestedInput>
---
+++ app/components/types/NestedString.ts
@@ -0,0 +1,1 @@
+export type NestedString = Array<string|NestedString>;
