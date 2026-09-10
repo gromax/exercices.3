cb5f8e001b2ee8440d67694835aa01cf38647679
gromax <gromaxx@gmail.com>
2026-09-10 21:36:12 +0200

    ajout d'un alea premier

--- app/components/exercices/tools/maths/misc/substitution.ts
+++ app/components/exercices/tools/maths/misc/substitution.ts
@@ -1,6 +1,17 @@
 import { TParams, InputType, NestedArray, NestedInput } from "@types"
 import MyMath from "../mymath";
 
+const PREMIERS = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+    103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+    233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353,
+    359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479,
+    487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617,
+    619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757,
+    761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907,
+    911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997
+]
+
 /**
  * Si chaine est de la forme @name.sub
  * avec name dans params, alors renvoie params[name][sub]
@@ -123,6 +134,7 @@ function _getAlea(sub:string): number | string {
     // s# où # es un entier >=0 -> alea entier entre -# et # exclu
     // S# où # es un entier >=0 -> alea flottant entre -# et # inclus, sans 0
     // v# où # est une chaine représentant des variables -> choisit aléatoirement la variable
+    // P# renvoie un nombe premier inférieur ou égal à #, # devant aller de 2 à 1000
     if (aType === 'v') {
         if (nStr.length === 0) {
             throw new Error("Liste de variables vide pour un paramètre aléatoire.")
@@ -147,6 +159,13 @@ function _getAlea(sub:string): number | string {
             return value;
         case 'I':
             return Math.floor(Math.random() * n) + 1
+        case 'P':
+            if (n < 2 || n > 1000) {
+                throw new Error(`[@__a.P${nStr}] l'indice peut aller de 2 à 1000 compris.`)
+            }
+            const primes = PREMIERS.filter(p => p <= n)
+            const i = Math.floor(Math.random() * primes.length)
+            return primes[i]
         default:
             return Math.floor(Math.random() * n)
     }
