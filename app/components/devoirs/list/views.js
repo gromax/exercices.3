a4e77e2c47bc6deee0f077cbc85f7fef73cdb2c1
gromax <gromaxx@gmail.com>
2026-09-15 21:12:50 +0200

    ajout des options pour masquer/afficher dans les cas archives ou pas + fonction de filtrage

--- app/components/devoirs/list/views.js
+++ app/components/devoirs/list/views.js
@@ -11,75 +11,94 @@ import devoirs_list_tpl from '@templates/devoirs/list/devoir-list.jst'
 
 
 const DevoirsPanel = View.extend({
-  adminMode: false,
-  showInactifs: true,
-  template: panel_tpl,
-  templateContext() {
-    return {
-      showAddButton: this.getOption("showAddButton"),
-    };
-  },
-  triggers: {
-    "click button.js-new": "devoir:new"
-  }
+    adminMode: false,
+    showInactifs: true,
+    template: panel_tpl,
+    templateContext() {
+        return {
+            showAddButton: this.getOption("showAddButton"),
+            archives: this.getOption("archives"),
+        };
+    },
+    triggers: {
+        "click button.js-new": "devoir:new"
+    }
 });
 
 const NoDevoirView = View.extend({
-  template: no_devoir_tpl,
-  tagName: "tr",
-  className: "alert"
+    template: no_devoir_tpl,
+    tagName: "tr",
+    className: "alert"
 });
 
 const DevoirItemView = View.extend({
-  adminMode: false,
-  tagName: "tr",
-  className() {
-    return this.model.get('notStarted')
-      ? "table-warning"
-      : (this.model.get('notEnded') ? "" : "table-danger");
-  },
-  template: devoir_item_tpl,
-  behaviors: [
-    DestroyWarn,
-    FlashItem
-  ],
+    adminMode: false,
+    tagName: "tr",
+    className() {
+        return this.model.get('notStarted')
+            ? "table-warning"
+            : (this.model.get('notEnded') ? "" : "table-danger");
+    },
+    template: devoir_item_tpl,
+    behaviors: [
+        DestroyWarn,
+        FlashItem
+    ],
 
-  triggers: {
-    "click .js-clone": "clone"
-  },
+    triggers: {
+        "click .js-clone": "clone",
+        "click .js-archive": "archive"
+    },
 
-  templateContext() {
-    return {
-      showNomOwner: this.getOption("showNomOwner")
-    };
-  }
+    templateContext() {
+        return {
+            showNomOwner: this.getOption("showNomOwner"),
+            showClasse: this.getOption("showClasse"),
+            showArchiveButton: this.getOption("showArchiveButton"),
+            showNotesButton: this.getOption("showNotesButton"),
+            showStatus: this.getOption("showStatus")
+        };
+    }
 });
 
 const DevoirsCollectionView = CollectionView.extend({
-  adminMode: false,
-  tagName: "table",
-  className: "table table-hover",
-  template: devoirs_list_tpl,
-  childView: DevoirItemView,
-  emptyView: NoDevoirView,
-  behaviors: [FilterList, SortList],
-  childViewEventPrefix: "item",
-  childViewContainer: "tbody",
-  filterKeys: ["nom", "nomProf"],
-  childViewOptions() {
-    return {
-      showNomOwner: this.getOption("showNomOwner")
-    };
-  },
-  templateContext() {
-    return {
-      showNomOwner: this.getOption("showNomOwner")
-    };
-  }
+    adminMode: false,
+    tagName: "table",
+    className: "table table-hover",
+    template: devoirs_list_tpl,
+    childView: DevoirItemView,
+    emptyView: NoDevoirView,
+    behaviors: [FilterList, SortList],
+    childViewEventPrefix: "item",
+    childViewContainer: "tbody",
+    filterKeys: ["nom", "nomProf"],
+    baseFilter(childModel) {
+        const archives = this.getOption("archives") || false
+        return (childModel.get("idClasse") === null) == archives
+    },
+    childViewOptions() {
+        return {
+            showNomOwner: this.getOption("showNomOwner"),
+            showClasse: this.getOption("showClasse"),
+            showArchiveButton: this.getOption("showArchiveButton"),
+            showNotesButton: this.getOption("showNotesButton"),
+            showStatus: this.getOption("showStatus")
+
+        };
+    },
+    templateContext() {
+        return {
+            showNomOwner: this.getOption("showNomOwner"),
+            showClasse: this.getOption("showClasse"),
+            showArchiveButton: this.getOption("showArchiveButton"),
+            showNotesButton: this.getOption("showNotesButton"),
+            showStatus: this.getOption("showStatus")
+        };
+    }
 });
 
 
 export {
-  DevoirsPanel,
-  DevoirsCollectionView
+    DevoirsPanel,
+    DevoirsCollectionView
 };
